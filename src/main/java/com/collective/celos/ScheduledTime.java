26efaa028744a4d69acdfe8a6fc08d4090859a62
Ivor Williams <ivor.williams@gmail.com>
2013-11-22 15:04:18 -0500

    Expose getters for year, month, day and hour, as formatted strings.  On reflection, I think this is the wrong way to go, but I'll commit it just in case we ever want to go this way.

--- src/main/java/com/collective/celos/ScheduledTime.java
+++ src/main/java/com/collective/celos/ScheduledTime.java
@@ -2,23 +2,51 @@ package com.collective.celos;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  * Scheduled time of a workflow run in UTC.
  */
 public class ScheduledTime extends ValueObject {
-    
+
+    private static final DateTimeFormatter YEAR_FORMATTER = DateTimeFormat
+            .forPattern("yyyy");
+    private static final DateTimeFormatter MONTH_FORMATTER = DateTimeFormat
+            .forPattern("MM");
+    private static final DateTimeFormatter DAY_FORMATTER = DateTimeFormat
+            .forPattern("dd");
+    private static final DateTimeFormatter HOUR_FORMATTER = DateTimeFormat
+            .forPattern("HH");
+
     protected final DateTime dateTime;
-    
+
     public ScheduledTime(String formattedDate) {
         this(DateTime.parse(formattedDate));
     }
-    
+
     public ScheduledTime(DateTime dateTime) {
         this.dateTime = Util.requireNonNull(dateTime);
         if (!dateTime.getZone().equals(DateTimeZone.UTC)) {
-            throw new IllegalArgumentException("Scheduled time must be in UTC, but isn't: " + dateTime);
+            throw new IllegalArgumentException(
+                    "Scheduled time must be in UTC, but isn't: " + dateTime);
         }
     }
-    
+
+    public String getYear() {
+        return dateTime.toString(YEAR_FORMATTER);
+    }
+
+    public String getMonth() {
+        return dateTime.toString(MONTH_FORMATTER);
+    }
+
+    public String getDay() {
+        return dateTime.toString(DAY_FORMATTER);
+    }
+
+    public String getHour() {
+        return dateTime.toString(HOUR_FORMATTER);
+    }
+
 }
---
+++ src/test/java/com/collective/celos/ScheduledTimeTest.java
@@ -0,0 +1,33 @@
+package com.collective.celos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ScheduledTimeTest {
+
+    @Test
+    public void testGetYear() {
+        ScheduledTime t = new ScheduledTime("2013-11-18T20:00Z");
+        assertEquals("2013", t.getYear());
+    }
+
+    @Test
+    public void testGetMonth() {
+        ScheduledTime t = new ScheduledTime("2013-11-18T20:00Z");
+        assertEquals("11", t.getMonth());
+    }
+
+    @Test
+    public void testGetDay() {
+        ScheduledTime t = new ScheduledTime("2013-11-18T20:00Z");
+        assertEquals("18", t.getDay());
+    }
+
+    @Test
+    public void testGetHour() {
+        ScheduledTime t = new ScheduledTime("2013-11-18T20:00Z");
+        assertEquals("20", t.getHour());
+    }
+
+}
