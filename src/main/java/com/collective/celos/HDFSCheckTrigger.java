3d761ede35fa52960d9992e039b17bb39b8c2a94
Ivor Williams <ivor.williams@gmail.com>
2013-11-22 20:00:56 -0500

    Implement HDFS trigger.

--- src/main/java/com/collective/celos/HDFSCheckTrigger.java
+++ src/main/java/com/collective/celos/HDFSCheckTrigger.java
@@ -1,12 +1,16 @@
 package com.collective.celos;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+/*
+ * Check in HDFS for a data dependency.
+ */
 public class HDFSCheckTrigger implements Trigger {
 
     // TODO: move this to somewhere that other code can access them?
@@ -26,14 +30,42 @@ public class HDFSCheckTrigger implements Trigger {
                 rawPathString, t);
 
         try {
-            Path path = new Path(cookedPathString);
+            /*
+             * We use unqualified paths for our triggers (that is, they don't
+             * specify the scheme of "hdfs"). 
+             * 
+             * For reasons I don't fully understand, this code treats these
+             * paths slightly differently depending on some run-time setting:
+             * 
+             * 1) In unit tests on a dev box and oj01, an unqualified path is
+             * treated as a local file.  That is, "fs" is an instance of
+             * LocalFileSystem.
+             * 
+             * 2) When run using "java" or "hadoop jar" on a dev box, the same
+             * is true.
+             * 
+             * 3) But, when run using "java" or "hadoop jar" on oj01, fs is in
+             * instance of DistributedFileSystem.
+             * 
+             * So, unit test are written to set up and test the local file
+             * system.  Celos itself can also be run locally, accessing the
+             * local file system.  But when run on oj01 (the likely deployment
+             * platform) this code will access paths from HDFS.
+             * 
+             * NOTE: this is the approach that Sibyl has been using since it
+             * was first deployed.
+             */
             Configuration conf = new Configuration();
-            FileSystem fs = path.getFileSystem(conf);
-            System.out.println("fs=" + fs);
+            FileSystem fs = FileSystem.get(URI.create(cookedPathString), conf);
+            
+            Path path = new Path(cookedPathString);
             boolean result = fs.exists(path);
+            
             return result;
+            
         } catch (IOException e) {
-            // TODO: Log exception, or throw?
+            // TODO: Log exception.
+            // TODO: Throw wrapping exception or simply return false?
             return false;
         }
     }
--- src/test/java/com/collective/celos/HDFSCheckTriggerTest.java
+++ src/test/java/com/collective/celos/HDFSCheckTriggerTest.java
@@ -1,13 +1,21 @@
 package com.collective.celos;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class HDFSCheckTriggerTest {
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    
     @Test(expected = NullPointerException.class)
     public void testNeedsPath() {
         Trigger trigger = new HDFSCheckTrigger();
@@ -15,7 +23,7 @@ public class HDFSCheckTriggerTest {
     }
 
     @Test
-    public void testDrectoryExists() {
+    public void testDirectoryExists() {
         Trigger trigger = new HDFSCheckTrigger();
         Properties props = new Properties();
         props.setProperty(HDFSCheckTrigger.PATH_KEY, "/tmp");
@@ -23,10 +31,41 @@ public class HDFSCheckTriggerTest {
     }
 
     @Test
-    public void testDrectoryDoesNotExist() {
+    public void testDirectoryDoesNotExist() {
         Trigger trigger = new HDFSCheckTrigger();
         Properties props = new Properties();
         props.setProperty(HDFSCheckTrigger.PATH_KEY, "/tmp-does-not-exist");
+        assertFalse(trigger.isDataAvailable(new ScheduledTime("2013-11-22T15:00Z"), props));
+    }
+
+    @Test
+    public void testFileExists() throws IOException {
+        String root = tempFolder.getRoot().getPath();
+        
+        File triggerFile = new File(root, "2013-11-22/1500/_READY");
+        triggerFile.getParentFile().mkdirs();
+        triggerFile.createNewFile();
+       
+        Trigger trigger = new HDFSCheckTrigger();
+        Properties props = new Properties();
+        props.setProperty(HDFSCheckTrigger.PATH_KEY, root + "/${year}-${month}-${day}/${hour}00/_READY");
+        
+        assertTrue(trigger.isDataAvailable(new ScheduledTime("2013-11-22T15:00Z"), props));
+    }
+
+    @Test
+    public void testFileDoesNotExist() {
+        String root = tempFolder.getRoot().getPath();
+        
+        File triggerFile = new File(root, "2013-11-22/1500/_READY");
+        if (triggerFile.exists()) {
+            triggerFile.delete(); // Make sure it doesn't exist
+        }
+       
+        Trigger trigger = new HDFSCheckTrigger();
+        Properties props = new Properties();
+        props.setProperty(HDFSCheckTrigger.PATH_KEY, root + "/${year}-${month}-${day}/${hour}00/_READY");
+        
         assertFalse(trigger.isDataAvailable(new ScheduledTime("2013-11-22T15:00Z"), props));
     }
 
