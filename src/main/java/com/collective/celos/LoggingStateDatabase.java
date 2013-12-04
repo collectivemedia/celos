185e32025e0ce3c8a488656ef2249ef72cefd0ad
Ivor Williams <ivor.williams@gmail.com>
2013-12-03 19:56:51 -0500

    Make SlotStateLogger injectable for DRYer testing.  Interaction-based tests for LoggingStateDatabase.

--- src/main/java/com/collective/celos/LoggingStateDatabase.java
+++ src/main/java/com/collective/celos/LoggingStateDatabase.java
@@ -9,12 +9,17 @@ import org.apache.log4j.Logger;
 public class LoggingStateDatabase implements StateDatabase {
 
     private final StateDatabase wrappedDatabase;
-    private final SlotStateLogger logger;
+    private final SlotStateLogger slotStateLogger;
 
     public LoggingStateDatabase(StateDatabase wrappedDatabase) {
+        this(wrappedDatabase, new SlotStateLogger(
+                Logger.getLogger(wrappedDatabase.getClass())));
+    }
+
+    LoggingStateDatabase(StateDatabase wrappedDatabase,
+            SlotStateLogger slotStateLogger) {
         this.wrappedDatabase = Util.requireNonNull(wrappedDatabase);
-        logger = new SlotStateLogger(Logger.getLogger(wrappedDatabase
-                .getClass()));
+        this.slotStateLogger = slotStateLogger;
     }
 
     @Override
@@ -22,20 +27,22 @@ public class LoggingStateDatabase implements StateDatabase {
         try {
             return wrappedDatabase.getSlotState(slot);
         } catch (Exception e) {
-            logger.logException(null, e);
+            slotStateLogger.logException(null, e);
             throw e;
         }
     }
 
     @Override
     public void putSlotState(SlotState state) throws Exception {
-        logger.info(state, "Changing status of slot "
-                + state.getSlotID() + " to " + state.getStatus()
-                + " with external ID = " + state.getExternalID());
+        slotStateLogger.info(
+                state,
+                "Changing status of slot " + state.getSlotID() + " to "
+                        + state.getStatus() + " with external ID = "
+                        + state.getExternalID());
         try {
             wrappedDatabase.putSlotState(state);
         } catch (Exception e) {
-            logger.logException(state, e);
+            slotStateLogger.logException(state, e);
             throw e;
         }
     }
--- src/test/java/com/collective/celos/LoggingStateDatabaseTest.java
+++ src/test/java/com/collective/celos/LoggingStateDatabaseTest.java
@@ -1,19 +1,15 @@
 package com.collective.celos;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class LoggingStateDatabaseTest {
-
-    @Rule
-    public TestingLogger testingLogger = new TestingLogger();
 
     private SlotID slotId;
     private SlotState slotState;
@@ -32,22 +28,24 @@ public class LoggingStateDatabaseTest {
 
     @Test
     public void putSlotState() throws Exception {
+        SlotStateLogger stateLogger = mock(SlotStateLogger.class);
         StateDatabase stateDatabase = new LoggingStateDatabase(
-                mock(StateDatabase.class));
+                mock(StateDatabase.class), stateLogger);
         stateDatabase.putSlotState(slotState);
 
-        assertEquals(
-                "[workflow-1@2013-12-03T13:00Z] Changing status of slot workflow-1@2013-12-03T13:00Z to RUNNING with external ID = 2398721837913-2767321868713-W",
-                testingLogger.getMessages()[0]);
+        verify(stateLogger)
+                .info(slotState,
+                        "Changing status of slot workflow-1@2013-12-03T13:00Z to RUNNING with external ID = 2398721837913-2767321868713-W");
+        verifyNoMoreInteractions(stateLogger);
     }
 
     @Test
-    public void putGetState() throws Exception {
+    public void getSlotState() throws Exception {
+        SlotStateLogger stateLogger = mock(SlotStateLogger.class);
         StateDatabase stateDatabase = new LoggingStateDatabase(
-                mock(StateDatabase.class));
+                mock(StateDatabase.class), stateLogger);
         stateDatabase.getSlotState(slotId);
-
-        assertEquals(0, testingLogger.getMessages().length);
+        verifyNoMoreInteractions(stateLogger);
     }
 
     @Test
@@ -56,41 +54,40 @@ public class LoggingStateDatabaseTest {
         Exception oops = new Exception("put failed");
         doThrow(oops).when(wrappedDatabase).putSlotState(slotState);
 
-        StateDatabase stateDatabase = new LoggingStateDatabase(wrappedDatabase);
+        SlotStateLogger stateLogger = mock(SlotStateLogger.class);
+        StateDatabase stateDatabase = new LoggingStateDatabase(wrappedDatabase,
+                stateLogger);
+
         try {
             stateDatabase.putSlotState(slotState);
         } catch (Exception e) {
             // Ignore
         }
 
-        assertEquals(oops.getStackTrace().length + 2,
-                testingLogger.getMessages().length);
-        assertTrue(testingLogger.getMessages()[0]
-                .contains("Changing status of slot"));
-        assertTrue(testingLogger.getMessages()[1].contains(oops.toString()));
-        for (String message : testingLogger.getMessages()) {
-            assertTrue(message.contains("[workflow-1@2013-12-03T13:00Z]"));
-        }
+        verify(stateLogger)
+                .info(slotState,
+                        "Changing status of slot workflow-1@2013-12-03T13:00Z to RUNNING with external ID = 2398721837913-2767321868713-W");
+        verify(stateLogger).logException(slotState, oops);
+        verifyNoMoreInteractions(stateLogger);
     }
 
     @Test
-    public void putGetStateException() throws Exception {
+    public void getSlotStateException() throws Exception {
         StateDatabase wrappedDatabase = mock(StateDatabase.class);
         Exception oops = new Exception("get failed");
         doThrow(oops).when(wrappedDatabase).getSlotState(slotId);
 
-        StateDatabase stateDatabase = new LoggingStateDatabase(wrappedDatabase);
+        SlotStateLogger stateLogger = mock(SlotStateLogger.class);
+        StateDatabase stateDatabase = new LoggingStateDatabase(wrappedDatabase,
+                stateLogger);
+
         try {
             stateDatabase.getSlotState(slotId);
         } catch (Exception e) {
             // Ignore
         }
 
-        assertEquals(oops.getStackTrace().length + 1,
-                testingLogger.getMessages().length);
-        assertTrue(testingLogger.getMessages()[0].contains(oops.toString()));
-        for (String message : testingLogger.getMessages()) {
-            assertTrue(message.contains("[none]"));
-        }
+        verify(stateLogger).logException(null, oops);
+        verifyNoMoreInteractions(stateLogger);
     }
 }
