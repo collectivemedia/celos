8eebbe899622a218d026195e7e87155686d2db94
Manuel Simoni <msimoni@gmail.com>
2013-12-12 20:16:15 +0100

    Add AbstractServlet class that synchronizes all database accesses.

---
+++ src/main/java/com/collective/celos/servlet/AbstractServlet.java
@@ -0,0 +1,28 @@
+package com.collective.celos.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Superclass for all servlets that access the database.
+ * 
+ * Serializes all database with a lock.
+ */
+@SuppressWarnings("serial")
+public abstract class AbstractServlet extends HttpServlet {
+
+    /**
+     * This lock serves to synchronize all operations.
+     */
+    protected static final Object LOCK = new Object();
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, java.io.IOException {
+        synchronized(LOCK) {
+            super.service(req, res);
+        }
+    }
+
+}
--- src/main/java/com/collective/celos/servlet/SchedulerServlet.java
+++ src/main/java/com/collective/celos/servlet/SchedulerServlet.java
@@ -3,7 +3,6 @@ package com.collective.celos.servlet;
 import java.io.File;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,30 +16,26 @@ import com.collective.celos.StateDatabase;
 import com.collective.celos.WorkflowConfiguration;
 import com.collective.celos.WorkflowConfigurationParser;
 
+/**
+ * Posting to this servlet triggers a scheduler step.
+ */
 @SuppressWarnings("serial")
-public class SchedulerServlet extends HttpServlet {
+public class SchedulerServlet extends AbstractServlet {
     
-    /**
-     * This lock serves to synchronize all scheduler operations.
-     */
-    public static final Object LOCK = new Object();
-
     private static final String CONFIG_PATH = "/etc/celos/workflows";
     private static final String DB_PATH = "/var/lib/celos/db";
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
-        synchronized(LOCK) {
-            try {
-                File configFile = new File(CONFIG_PATH);
-                WorkflowConfiguration config =
-                        new WorkflowConfigurationParser().parseConfiguration(configFile);
-                StateDatabase db = new FileSystemStateDatabase(new File(DB_PATH));
-                int slidingWindowHours = 24 * 7;
-                ScheduledTime current = new ScheduledTime(DateTime.now(DateTimeZone.UTC));
-                new Scheduler(config, db, slidingWindowHours).step(current);
-            } catch(Exception e) {
-                throw new ServletException(e);
-            }
+        try {
+            File configFile = new File(CONFIG_PATH);
+            WorkflowConfiguration config =
+                    new WorkflowConfigurationParser().parseConfiguration(configFile);
+            StateDatabase db = new FileSystemStateDatabase(new File(DB_PATH));
+            int slidingWindowHours = 24 * 7;
+            ScheduledTime current = new ScheduledTime(DateTime.now(DateTimeZone.UTC));
+            new Scheduler(config, db, slidingWindowHours).step(current);
+        } catch(Exception e) {
+            throw new ServletException(e);
         }
     }
 
