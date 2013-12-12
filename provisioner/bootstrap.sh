1506a43e8e508431c589db6fb94ee0eef2e7e38d
Sarguru Nathan <smohan@collective.com>
2013-12-12 18:21:20 +0530

    initial commit for provisioner

---
+++ provisioner/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "creating security groups now ...."
+./sec_groups.rb
+echo "creating machines"
+./aws_provision_dysec.rb --create-cluster
+echo "Done creating"
+./aws_provision_dysec.rb --list
+sleep 120
+rm ./tmp/hosts
+echo "preprovisioning stage"
+./aws_provision_dysec.rb --start-cluster
+echo "Provisioning now, passing it on to ansible"
+ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 --skip-tags=oozie 
+ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 -t oozie 
+
