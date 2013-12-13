a9b31cedf76d2dfa24125d6c8ce4aaa1d4ae1249
Manuel Simoni <msimoni@gmail.com>
2013-12-13 13:19:57 +0100

    Don't run ansible verbosely.

--- scripts/cluster-deploy.sh
+++ scripts/cluster-deploy.sh
@@ -4,4 +4,4 @@ set -x
 
 # Deploy currently checked-out local source on test cluster
 
-ansible-playbook -v -i provisioner/tmp/inventory -u ubuntu provisioner/celos_deploy.yaml
+ansible-playbook -i provisioner/tmp/inventory -u ubuntu provisioner/celos_deploy.yaml
