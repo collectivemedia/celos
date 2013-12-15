f32be4d6b7aa7d2de9c7016128774d5b45e5f78b
Manuel Simoni <msimoni@gmail.com>
2013-12-15 20:40:13 +0100

    Run ansible with -u celos.

--- scripts/cluster-test.sh
+++ scripts/cluster-test.sh
@@ -4,4 +4,4 @@ set -x
 
 # Run integration tests against currently checkout local source on test cluster.
 
-ansible-playbook -i provisioner/tmp/inventory -u ubuntu provisioner/celos_test.yaml
+ansible-playbook -i provisioner/tmp/inventory -u celos provisioner/celos_test.yaml
