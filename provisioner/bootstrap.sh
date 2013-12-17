90759f0cf9b777f9e42dae443adede286bcc2fd6
Sarguru Nathan <smohan@collective.com>
2013-12-17 15:57:40 +0530

    add hive and pig roles

--- provisioner/bootstrap.sh
+++ provisioner/bootstrap.sh
@@ -11,6 +11,6 @@ rm ./tmp/hosts
 echo "preprovisioning stage"
 ./aws_provision_dysec.rb --start-cluster
 echo "Provisioning now, passing it on to ansible"
-ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 --skip-tags=oozie,yarn 
-ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 -t oozie,yarn 
+ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 --skip-tags=oozie,yarn,hive 
+ansible-playbook -i tmp/inventory -u ubuntu site.yaml  -f 4 -t oozie,yarn,hive 
 
