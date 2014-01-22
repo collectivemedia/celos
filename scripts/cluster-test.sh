add6759e150106dc54e452fed7f4556382d7b318
Alexander Konopko <alexander.konopko@gmail.com>
2014-01-22 15:39:28 +0200

    complete test script

--- scripts/cluster-test.sh
+++ scripts/cluster-test.sh
@@ -6,7 +6,7 @@ set -x
 
 ansible-playbook -i provisioner/tmp/inventory -u celos provisioner/celos_test.yaml
 ansible-playbook -i provisioner/tmp/inventory -u celos  scripts/test/test-workflow.yaml --extra-vars "workflow=file-copy sample_time=2013-12-20T20:00Z"
-! ansible-playbook -i provisioner/tmp/inventory -u celos  scripts/test/test-workflow.yaml --extra-vars "workflow=file-copy-with-bad-output sample_time=2013-12-20T20:00Z"
-! ansible-playbook -i provisioner/tmp/inventory -u celos  scripts/test/test-workflow.yaml --extra-vars "workflow=file-copy-with-missing-output sample_time=2013-12-20T20:00Z" 
+ansible-playbook -i provisioner/tmp/inventory -u celos  scripts/test/test-workflow.yaml --extra-vars "workflow=file-copy-with-bad-output sample_time=2013-12-20T20:00Z" && false
+ansible-playbook -i provisioner/tmp/inventory -u celos  scripts/test/test-workflow.yaml --extra-vars "workflow=file-copy-with-missing-output sample_time=2013-12-20T20:00Z" && false
 
 echo You win! All tests OK.
