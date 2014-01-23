e33feef38944f610b8bf0441a43e1b4d7a8a6cd0
Alexander Konopko <alexander.konopko@gmail.com>
2014-01-23 19:20:37 +0200

    change indentation

--- samples/wordcount/src/main/java/com/collective/celos/examples/wordcount/WordCount.java
+++ samples/wordcount/src/main/java/com/collective/celos/examples/wordcount/WordCount.java
@@ -12,46 +12,46 @@ import org.apache.hadoop.util.*;
 public class WordCount {
 
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-      private final static IntWritable one = new IntWritable(1);
-      private Text word = new Text();
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-      public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        String line = value.toString();
-        StringTokenizer tokenizer = new StringTokenizer(line);
-        while (tokenizer.hasMoreTokens()) {
-          word.set(tokenizer.nextToken());
-          output.collect(word, one);
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
         }
-      }
     }
 
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-      public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        int sum = 0;
-        while (values.hasNext()) {
-          sum += values.next().get();
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
         }
-        output.collect(key, new IntWritable(sum));
-      }
     }
 
     public static void main(String[] args) throws Exception {
-      JobConf conf = new JobConf(WordCount.class);
-      conf.setJobName("wordcount");
+        JobConf conf = new JobConf(WordCount.class);
+        conf.setJobName("wordcount");
 
-      conf.setOutputKeyClass(Text.class);
-      conf.setOutputValueClass(IntWritable.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
 
-      conf.setMapperClass(Map.class);
-      conf.setCombinerClass(Reduce.class);
-      conf.setReducerClass(Reduce.class);
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
 
-      conf.setInputFormat(TextInputFormat.class);
-      conf.setOutputFormat(TextOutputFormat.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-      FileInputFormat.setInputPaths(conf, new Path(args[0]));
-      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-      JobClient.runJob(conf);
+        JobClient.runJob(conf);
     }
 }
