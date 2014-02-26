f508fbe64000226387205a990ec77f528c8abe3e
Manuel Simoni <msimoni@gmail.com>
2014-02-26 16:03:18 +0100

    Factor creation of objects from JSON configuration into JSONInstanceCreator class to enable reuse by AndTrigger.

---
+++ src/main/java/com/collective/celos/JSONInstanceCreator.java
@@ -0,0 +1,58 @@
+package com.collective.celos;
+
+import java.lang.reflect.Constructor;
+
+import org.apache.commons.beanutils.ConstructorUtils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Creates instances of various classes from a JSON configuration.
+ * 
+ * Every object must have a "type" property (string) containing its Java class.
+ * 
+ * Additionally, it may have a "properties" property (JSON object) containing additional properties.
+ * 
+ * The instance creator invokes the class' constructor with a ObjectNode as argument,
+ * containing the additional properties (empty if no additional properties are in the JSON).
+ */
+public class JSONInstanceCreator {
+
+    private static final String TYPE_PROP = "type";
+    private static final String PROPERTIES_PROP = "properties";
+    
+    public Object createInstance(JsonNode node) throws Exception {
+        ObjectNode properties = getProperties(node);
+        Constructor<?> ctor = getConstructor(node);
+        return ctor.newInstance(properties);
+    }
+
+    ObjectNode getProperties(JsonNode node) {
+        JsonNode properties = node.get(PROPERTIES_PROP);
+        if (properties == null) {
+            return Util.newObjectNode();
+        } else {
+            if (!properties.isObject()) {
+                throw new IllegalArgumentException("Properties must be an object, but is: " + properties);
+            } else {
+                return (ObjectNode) properties;
+            }
+        }
+    }
+
+    private Constructor<?> getConstructor(JsonNode workflowNode) throws ClassNotFoundException {
+        JsonNode typeNode = workflowNode.get(TYPE_PROP);
+        if (typeNode == null || !typeNode.isTextual()) {
+            throw new IllegalArgumentException("Type must be a string: " + workflowNode.toString());
+        }
+        String className = typeNode.textValue();
+        Class<?> c = Class.forName(className);
+        Constructor<?> ctor = ConstructorUtils.getAccessibleConstructor(c, ObjectNode.class);
+        if (ctor == null) {
+            throw new RuntimeException("Constructor with Properties argument not found for " + className);
+        }
+        return ctor;
+    }
+    
+}
--- src/main/java/com/collective/celos/WorkflowConfigurationParser.java
+++ src/main/java/com/collective/celos/WorkflowConfigurationParser.java
@@ -1,24 +1,19 @@
 package com.collective.celos;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
 import java.util.Collection;
 
-import org.apache.commons.beanutils.ConstructorUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * Reads a set of JSON files from a directory and creates a WorkflowConfiguration.
  */
 public class WorkflowConfigurationParser {
 
-    private static final String PROPERTIES_PROP = "properties";
-    private static final String TYPE_PROP = "type";
     private static final String EXTERNAL_SERVICE_PROP = "externalService";
     private static final String TRIGGER_PROP = "trigger";
     private static final String SCHEDULING_STRATEGY_PROP = "schedulingStrategy";
@@ -27,6 +22,8 @@ public class WorkflowConfigurationParser {
     private static final String MAX_RETRY_COUNT_PROP = "maxRetryCount";
 
     private static final Logger LOGGER = Logger.getLogger(WorkflowConfigurationParser.class);
+    
+    private final JSONInstanceCreator creator = new JSONInstanceCreator();
     
     public WorkflowConfiguration parseConfiguration(File dir) throws Exception {
         LOGGER.info("Workflow configuration directory: " + dir);
@@ -63,19 +60,27 @@ public class WorkflowConfigurationParser {
     }
 
     private ExternalService getExternalServiceFromJSON(WorkflowID id, JsonNode workflowNode) throws Exception {
-        return (ExternalService) createInstance(id, workflowNode.get(EXTERNAL_SERVICE_PROP));
+        LOGGER.info("Creating external service for: " + id);
+        return (ExternalService) createInstance(workflowNode.get(EXTERNAL_SERVICE_PROP));
     }
 
     private Trigger getTriggerFromJSON(WorkflowID id, JsonNode workflowNode) throws Exception {
-        return (Trigger) createInstance(id, workflowNode.get(TRIGGER_PROP));
+        LOGGER.info("Creating trigger for: " + id);
+        return (Trigger) createInstance(workflowNode.get(TRIGGER_PROP));
     }
 
     private SchedulingStrategy getSchedulingStrategyFromJSON(WorkflowID id, JsonNode workflowNode) throws Exception {
-        return (SchedulingStrategy) createInstance(id, workflowNode.get(SCHEDULING_STRATEGY_PROP));
+        LOGGER.info("Creating scheduling strategy for: " + id);
+        return (SchedulingStrategy) createInstance(workflowNode.get(SCHEDULING_STRATEGY_PROP));
     }
 
     private Schedule getScheduleFromJSON(WorkflowID id, JsonNode workflowNode) throws Exception {
-        return (Schedule) createInstance(id, workflowNode.get(SCHEDULE_PROP));
+        LOGGER.info("Creating schedule for: " + id);
+        return (Schedule) createInstance(workflowNode.get(SCHEDULE_PROP));
+    }
+
+    private Object createInstance(JsonNode jsonNode) throws Exception {
+        return creator.createInstance(jsonNode);
     }
 
     private WorkflowID getWorkflowID(JsonNode workflowNode) {
@@ -84,40 +89,6 @@ public class WorkflowConfigurationParser {
             throw new IllegalArgumentException("ID must be a string: " + workflowNode.toString());
         }
         return new WorkflowID(idNode.textValue());
-    }
-
-    private Object createInstance(WorkflowID id, JsonNode node) throws Exception {
-        ObjectNode properties = getProperties(node);
-        Constructor<?> ctor = getConstructor(node);
-        LOGGER.info("Instantiating " + ctor + " for: " + id);
-        return ctor.newInstance(properties);
-    }
-
-    ObjectNode getProperties(JsonNode node) {
-        JsonNode properties = node.get(PROPERTIES_PROP);
-        if (properties == null) {
-            return Util.newObjectNode();
-        } else {
-            if (!properties.isObject()) {
-                throw new IllegalArgumentException("Properties must be an object, but is: " + properties);
-            } else {
-                return (ObjectNode) properties;
-            }
-        }
-    }
-
-    private Constructor<?> getConstructor(JsonNode workflowNode) throws ClassNotFoundException {
-        JsonNode typeNode = workflowNode.get(TYPE_PROP);
-        if (typeNode == null || !typeNode.isTextual()) {
-            throw new IllegalArgumentException("Type must be a string: " + workflowNode.toString());
-        }
-        String className = typeNode.textValue();
-        Class<?> c = Class.forName(className);
-        Constructor<?> ctor = ConstructorUtils.getAccessibleConstructor(c, ObjectNode.class);
-        if (ctor == null) {
-            throw new RuntimeException("Constructor with Properties argument not found for " + className);
-        }
-        return ctor;
     }
 
 }
