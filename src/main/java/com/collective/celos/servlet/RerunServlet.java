f5ca0814b5aff77686145c0f98c31451c7937754
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-03-04 20:37:27 +0200

    return updated SlotState in "rerun" call

--- src/main/java/com/collective/celos/servlet/RerunServlet.java
+++ src/main/java/com/collective/celos/servlet/RerunServlet.java
@@ -10,18 +10,23 @@ import com.collective.celos.SlotID;
 import com.collective.celos.SlotState;
 import com.collective.celos.StateDatabase;
 import com.collective.celos.WorkflowID;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * Posting to this servlet reruns the specified slot.
- * 
+ *
+ * POST /rerun
+ *
  * Parameters:
  * 
- * id -- workflow ID
+ * id -- workflow ID. Example: "foo-bar-workflow"
  * 
- * time -- scheduled time of slot
+ * time -- scheduled time of slot in Zulu time format. Example: "2014-02-25T22:00:00.000Z"
+ *
+ * Returns updated state of the slot
  */
 @SuppressWarnings("serial")
-public class RerunServlet extends AbstractServlet {
+public class RerunServlet extends AbstractJSONServlet {
     
     private static final String ID_PARAM = "id";
     
@@ -34,19 +39,29 @@ public class RerunServlet extends AbstractServlet {
             }
             SlotID slot = new SlotID(new WorkflowID(id), time);
             StateDatabase db = new SchedulerConfiguration().makeDefaultStateDatabase();
-            updateSlotToRerun(slot, db);
+            SlotState slotState = updateSlotToRerun(slot, db);
+
+            ObjectNode node = getJSONNode(slotState);
+            writer.writeValue(res.getOutputStream(), node);
         } catch(Exception e) {
             throw new ServletException(e);
         }
     }
 
-    void updateSlotToRerun(SlotID slot, StateDatabase db) throws Exception {
+    private ObjectNode getJSONNode(SlotState slotState) {
+        ObjectNode node = mapper.createObjectNode();
+        node.put(slotState.getScheduledTime().toString(), slotState.toJSONNode());
+        return node;
+    }
+
+    SlotState updateSlotToRerun(SlotID slot, StateDatabase db) throws Exception {
         SlotState state = db.getSlotState(slot);
         if (state == null) {
             throw new IllegalArgumentException("Slot not found: " + slot);
         }
         SlotState newState = state.transitionToRerun();
         db.putSlotState(newState);
+        return newState;
     }
 
 }
