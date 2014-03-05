8361442338c4bb44983f7e6a4111e89e115f79f5
Manuel Simoni <msimoni@gmail.com>
2014-03-05 12:18:00 +0100

    Update wordcount sample to use new syntax.

--- samples/wordcount/wordcount.js
+++ samples/wordcount/wordcount.js
@@ -1,29 +1,28 @@
 addWorkflow({
+
     "id": "wordcount",
-    "schedule": {
-        "type": "com.collective.celos.HourlySchedule"
-    },
-    "schedulingStrategy": {
-        "type": "com.collective.celos.SerialSchedulingStrategy"
-    },
-    "trigger": {
-        "type": "com.collective.celos.HDFSCheckTrigger",
-        "properties": {
-            "celos.hdfs.fs": "hdfs://nn",
-            "celos.hdfs.path": "/user/celos/samples/wordcount/input/${year}-${month}-${day}T${hour}00.txt"
-        }
-    },
-    "externalService": {
-        "type": "com.collective.celos.OozieExternalService",
-        "properties": {
+
+    "maxRetryCount": 0,
+
+    "schedule": hourlySchedule(),
+
+    "schedulingStrategy": serialSchedulingStrategy(),
+
+    "trigger": hdfsCheckTrigger(
+        "/user/celos/samples/wordcount/input/${year}-${month}-${day}T${hour}00.txt",
+        "hdfs://nn"
+    ),
+
+    "externalService": oozieExternalService(
+        {
             "nameNode": "hdfs://nn",
             "jobTracker": "hdfs://nn",
-            "celos.oozie.url": "http://nn:11000/oozie",
             "user.name": "celos",
             "oozie.wf.application.path": "/user/celos/samples/wordcount/workflow/workflow.xml",
             "inputDir": "/user/celos/samples/wordcount/input",
             "outputDir": "/user/celos/samples/wordcount/output"
-        }
-    },
-    "maxRetryCount": 0
+        },
+        "http://nn:11000/oozie"
+    )
+
 });
