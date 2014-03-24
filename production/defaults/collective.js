ab5a8f6cfdf5735e88ff3b8843b97582e1a5fae6
Manuel Simoni <msimoni@gmail.com>
2014-03-24 16:52:34 +0100

    Add NAME_NODE default.

--- production/defaults/collective.js
+++ production/defaults/collective.js
@@ -4,6 +4,8 @@ var CELOS_DEFAULT_OOZIE = "http://admin1.ny7.collective-media.net:11000/oozie";
 
 var CELOS_DEFAULT_HDFS = "hdfs://nameservice1";
 
+var NAME_NODE = CELOS_DEFAULT_HDFS;
+
 var JOB_TRACKER = "admin1.ny7.collective-media.net:8032";
 
 var HIVE_METASTORE = "thrift://admin1.ny7.collective-media.net:9083";
