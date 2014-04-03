da5ac93f4d79234bb54afc16b52d61b8f62cc66f
Manuel Simoni <msimoni@gmail.com>
2014-04-03 18:33:37 +0200

    Update Oozie and Hive URLs.

--- production/defaults/collective.js
+++ production/defaults/collective.js
@@ -1,6 +1,6 @@
 var CELOS_USER = "celos";
 
-var CELOS_DEFAULT_OOZIE = "http://admin1.ny7.collective-media.net:11000/oozie";
+var CELOS_DEFAULT_OOZIE = "http://oozie002.ny7.collective-media.net:11000/oozie";
 
 var CELOS_DEFAULT_HDFS = "hdfs://nameservice1";
 
@@ -8,7 +8,7 @@ var NAME_NODE = CELOS_DEFAULT_HDFS;
 
 var JOB_TRACKER = "admin1.ny7.collective-media.net:8032";
 
-var HIVE_METASTORE = "thrift://admin1.ny7.collective-media.net:9083";
+var HIVE_METASTORE = "thrift://hive-meta.ny7.collective-media.net:9803";
 
 var CELOS_DEFAULT_OOZIE_PROPERTIES = {
     "user.name": CELOS_USER,
