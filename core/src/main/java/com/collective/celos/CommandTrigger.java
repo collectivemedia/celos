ab2a2778312735bcec5b72e7d3f8aa4994415f58
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-04-14 15:25:33 +0300

    #208: remove wrong import

--- core/src/main/java/com/collective/celos/CommandTrigger.java
+++ core/src/main/java/com/collective/celos/CommandTrigger.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.text.StrBuilder;
 import org.apache.log4j.Logger;
 
 public class CommandTrigger implements Trigger {
