9f4ef53a6a6139448ea9bf2686a25288c0c48cb4
Manuel Simoni <msimoni@gmail.com>
2014-04-22 16:09:31 +0200

    Implement {Hourly,Minutely}Schedule as CronSchedules.

--- src/main/java/com/collective/celos/HourlySchedule.java
+++ src/main/java/com/collective/celos/HourlySchedule.java
@@ -1,33 +1,9 @@
 package com.collective.celos;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-import org.joda.time.DateTime;
-
-public class HourlySchedule implements Schedule {
+public class HourlySchedule extends CronSchedule {
 
     public HourlySchedule() {
+        super("0 0 * * * ?");
     }
     
-    @Override
-    public SortedSet<ScheduledTime> getScheduledTimes(ScheduledTime start, ScheduledTime end) {
-        DateTime startDT = start.getDateTime();
-        DateTime endDT = end.getDateTime();
-        DateTime hour;
-        // If start time of window is full hour, use it as first hour
-        // otherwise use next full hour as first one.
-        if (Util.isFullHour(startDT)) {
-            hour = startDT;
-        } else {
-            hour = Util.toFullHour(startDT.plusHours(1));
-        }
-        SortedSet<ScheduledTime> hours = new TreeSet<ScheduledTime>();
-        while(hour.isBefore(endDT)) {
-            hours.add(new ScheduledTime(hour));
-            hour = hour.plusHours(1);
-        }
-        return hours;
-    }
-
 }
--- src/main/java/com/collective/celos/MinutelySchedule.java
+++ src/main/java/com/collective/celos/MinutelySchedule.java
@@ -1,33 +1,9 @@
 package com.collective.celos;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-import org.joda.time.DateTime;
-
-public class MinutelySchedule implements Schedule {
+public class MinutelySchedule extends CronSchedule {
 
     public MinutelySchedule() {
+        super("0 * * * * ?");
     }
     
-    @Override
-    public SortedSet<ScheduledTime> getScheduledTimes(ScheduledTime start, ScheduledTime end) {
-        DateTime startDT = start.getDateTime();
-        DateTime endDT = end.getDateTime();
-        DateTime minute;
-        // If start time of window is full minute, use it as first minute
-        // otherwise use next full minute as first one.
-        if (Util.isFullMinute(startDT)) {
-            minute = startDT;
-        } else {
-            minute = Util.toFullMinute(startDT.plusMinutes(1));
-        }
-        SortedSet<ScheduledTime> minutes = new TreeSet<ScheduledTime>();
-        while(minute.isBefore(endDT)) {
-            minutes.add(new ScheduledTime(minute));
-            minute = minute.plusMinutes(1);
-        }
-        return minutes;
-    }
-
 }
