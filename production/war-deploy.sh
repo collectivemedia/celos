f48c1b4c2993ae7047fae1bdbe60e02226a83015
Manuel Simoni <msimoni@gmail.com>
2014-09-22 18:41:06 +0200

    Re-add sudo for starting/stopping Tomcat.

--- production/war-deploy.sh
+++ production/war-deploy.sh
@@ -4,7 +4,7 @@ set -x
 
 # Deploy WAR to local Tomcat
 
-/etc/init.d/tomcat6 stop
+sudo /etc/init.d/tomcat6 stop
 rm -rf /var/lib/tomcat6/webapps/celos
 cp target/celos-1.0.0.war /var/lib/tomcat6/webapps/celos.war
-/etc/init.d/tomcat6 start
+sudo /etc/init.d/tomcat6 start
