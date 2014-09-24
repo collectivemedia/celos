dcfa3827911d50bd0a6e766970b61e9050dc10b6
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-09-24 18:47:33 +0400

    little cosmetic changes

--- samples/wordcount/src/main/celos/workflow.js
+++ samples/wordcount/src/main/celos/workflow.js
@@ -1,19 +1,16 @@
 importDefaults("collective");
 
-function myOozieProps(slotId) {
-    return {
-        "oozie.wf.application.path": hdfsPath("/user/celos/app/wordcount/workflow.xml"),
-        "inputDir": hdfsPath("/user/celos/app/wordcount/input"),
-        "outputDir": hdfsPath("/user/celos/app/wordcount/output")
-    }
-}
-
 addWorkflow({
     "id": "wordcount",
     "maxRetryCount": 0,
     "schedule": hourlySchedule(),
     "schedulingStrategy": serialSchedulingStrategy(),
-    "trigger": hdfsCheckTrigger(hdfsPath("/user/celos/app/wordcount/input/${year}-${month}-${day}T${hour}00.txt")),
-    "externalService": oozieExternalService(myOozieProps)
-
+    "trigger": hdfsCheckTrigger(hdfsPath("/input/wordcount/${year}-${month}-${day}T${hour}00.txt")),
+    "externalService": oozieExternalService(function (slotId) {
+        return {
+            "oozie.wf.application.path": hdfsPath("/user/celos/app/wordcount/workflow.xml"),
+            "inputDir": hdfsPath("/input/wordcount"),
+            "outputDir": hdfsPath("/output/wordcount")
+        }
+    })
 });
