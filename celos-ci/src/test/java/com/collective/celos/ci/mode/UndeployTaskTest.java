c44812a786a36c386c51b1831d3416c61fb6ed57
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-10-01 23:07:35 +0300

    restore undeploy config test

---
+++ celos-ci/src/test/java/com/collective/celos/ci/mode/UndeployTaskTest.java
@@ -0,0 +1,62 @@
+package com.collective.celos.ci.mode;
+
+import com.collective.celos.ci.config.CelosCiCommandLine;
+import com.collective.celos.ci.config.deploy.CelosCiContext;
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.util.Set;
+
+/**
+ * Created by akonopko on 10/1/14.
+ */
+public class UndeployTaskTest {
+
+    @Rule
+    public TemporaryFolder tempDir = new TemporaryFolder();
+
+    @Test
+    public void testCelosCiUndeployContext() throws Exception {
+
+
+        String hadoopCoreUrl = Thread.currentThread().getContextClassLoader().getResource("com/collective/celos/ci/testing/config/core-site.xml").getFile();
+        String hadoopHdfsUrl = Thread.currentThread().getContextClassLoader().getResource("com/collective/celos/ci/testing/config/hdfs-site.xml").getFile();
+
+        String targetFileStr = "{\n" +
+                "    \"security.settings\": \"secsettings\",\n" +
+                "    \"celos.workflow.dir\": \"celoswfdir\",\n" +
+                "    \"hadoop.hdfs-site.xml\": \"" + hadoopHdfsUrl +"\",\n" +
+                "    \"hadoop.core-site.xml\": \"" + hadoopCoreUrl +"\",\n" +
+                "    \"defaults.file.uri\": \"deffile\"\n" +
+                "}\n";
+
+        File targetFile = tempDir.newFile();
+        FileOutputStream stream = new FileOutputStream(targetFile);
+        stream.write(targetFileStr.getBytes());
+        stream.flush();
+
+        CelosCiCommandLine commandLine = new CelosCiCommandLine(targetFile.toURI().toString(), "UNDEPLOY", "deploydir", "workflow", "testDir", "uname");
+        UndeployTask celosCiDeploy = new UndeployTask(commandLine);
+
+        CelosCiContext context = celosCiDeploy.getCiContext();
+
+        Assert.assertEquals(context.getDeployDir(), commandLine.getDeployDir());
+        Assert.assertEquals(context.getHdfsPrefix(), "");
+        Assert.assertEquals(context.getMode(), commandLine.getMode());
+        Assert.assertEquals(context.getTarget().getCelosWorkflowsDirUri(), URI.create("celoswfdir"));
+        Assert.assertEquals(context.getTarget().getDefaultsFile(), URI.create("deffile"));
+        Assert.assertEquals(context.getTarget().getPathToCoreSite(), URI.create(hadoopCoreUrl));
+        Assert.assertEquals(context.getTarget().getPathToHdfsSite(), URI.create(hadoopHdfsUrl));
+        Assert.assertEquals(context.getUserName(), commandLine.getUserName());
+        Assert.assertEquals(context.getWorkflowName(), commandLine.getWorkflowName());
+    }
+
+}
