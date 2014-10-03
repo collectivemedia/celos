9ccf1bbaa4c6d9e32b082a00b9a8a9ed2c0ccad3
Manuel Simoni <msimoni@gmail.com>
2014-10-03 15:03:50 +0200

    Delete test-wordcount.sh

--- scripts/test-wordcount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-(cd samples/wordcount; gradle build deploy_dir)
-gradle build
-java -jar build/libs/celos-ci-2.0.jar com.collective.celos.ci.CelosCi --deployDir build/celos_deploy --target sftp://celos001.ny7.collective-media.net/home/akonopko/target.json --workflowName wordcount  --mode TEST --testDir src/test/celos-ci
