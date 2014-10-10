e64ff94a8e5d9f32e9a20164eecb41e620ac8ffb
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-10-10 20:23:00 +0300

    add compare stuff

---
+++ celos-ci/src/test/java/com/collective/celos/ci/testing/fixtures/compare/FixObjectCompareResultTest.java
@@ -0,0 +1,65 @@
+package com.collective.celos.ci.testing.fixtures.compare;
+
+import com.collective.celos.ci.testing.structure.tree.AbstractTreeObjectProcessor;
+import com.collective.celos.ci.testing.structure.tree.TreeObject;
+import com.collective.celos.ci.testing.structure.tree.TreeStructureProcessor;
+import com.google.common.collect.Maps;
+import junit.framework.Assert;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.text.StrBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by akonopko on 10/8/14.
+ */
+public class FixObjectCompareResultTest {
+
+    @Test
+    public void testDescription() throws IOException {
+        Map<String, FixObjectCompareResult> children = Maps.newHashMap();
+        FixObjectCompareResult e1 = FixObjectCompareResult.failed("fail");
+        FixObjectCompareResult e2 = FixObjectCompareResult.success();
+        children.put("path1", e1);
+        children.put("path2", e2);
+        FixObjectCompareResult result = FixObjectCompareResult.wrapFailed(children, null);
+        Assert.assertEquals(e2.getStatus(), FixObjectCompareResult.Status.SUCCESS);
+        Assert.assertEquals(result.getStatus(), FixObjectCompareResult.Status.FAIL);
+        Assert.assertEquals(result.generateDescription(), "path1 : fail\n");
+    }
+
+    @Test
+    public void testDescriptionWithMessage() throws IOException {
+        Map<String, FixObjectCompareResult> children = Maps.newHashMap();
+        FixObjectCompareResult e1 = FixObjectCompareResult.failed("fail");
+        FixObjectCompareResult e2 = FixObjectCompareResult.success();
+        children.put("path1", e1);
+        children.put("path2", e2);
+        FixObjectCompareResult result = FixObjectCompareResult.wrapFailed(children, "my own error");
+        Assert.assertEquals(result.getStatus(), FixObjectCompareResult.Status.FAIL);
+        Assert.assertEquals(result.generateDescription(), "my own error\n" +
+                "path1 : fail\n");
+    }
+
+    @Test
+    public void testDescriptionWithMessageMoreLayers() throws IOException {
+        Map<String, FixObjectCompareResult> c1 = Maps.newHashMap();
+        FixObjectCompareResult e1 = FixObjectCompareResult.failed("fail");
+        FixObjectCompareResult e2 = FixObjectCompareResult.success();
+        c1.put("path1", e1);
+        c1.put("path2", e2);
+        FixObjectCompareResult e3 = FixObjectCompareResult.wrapFailed(c1, "my own error");
+
+        Map<String, FixObjectCompareResult> c2 = Maps.newHashMap();
+        c2.put("path3", e3);
+        FixObjectCompareResult result = FixObjectCompareResult.wrapFailed(c2, null);
+        Assert.assertEquals(result.getStatus(), FixObjectCompareResult.Status.FAIL);
+        Assert.assertEquals(result.generateDescription(), "path3/path1 : fail\n" +
+                "path3 : my own error\n");
+    }
+
+}
