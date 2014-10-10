ee4d65eb7328f5b07ddb399cf54ea10a32b21219
Alexander Konopko <alexander.konopko@sigmaukraine.com>
2014-10-10 20:35:38 +0300

    add deploy stuff

---
+++ celos-ci/src/main/java/com/collective/celos/ci/testing/fixtures/deploy/HdfsFixtureDeployer.java
@@ -0,0 +1,21 @@
+package com.collective.celos.ci.testing.fixtures.deploy;
+
+import com.collective.celos.ci.config.deploy.CelosCiContext;
+import com.collective.celos.ci.testing.structure.fixobject.FixDir;
+import com.collective.celos.ci.testing.structure.tree.TreeStructureProcessor;
+
+/**
+ * Created by akonopko on 10/7/14.
+ */
+public class HdfsFixtureDeployer implements FixDirDeployer {
+
+    private final CelosCiContext context;
+
+    public HdfsFixtureDeployer(CelosCiContext context) {
+        this.context = context;
+    }
+
+    public void deploy(FixDir fixDir) throws Exception {
+        new TreeStructureProcessor().process(fixDir, new HdfsFixtureDeployerFileProcessor(context));
+    }
+}
