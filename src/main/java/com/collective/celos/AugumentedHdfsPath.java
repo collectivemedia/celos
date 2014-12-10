29c374a036f1ebd38cd2b1c45f616a7fe18cdf93
Alexander Konopko <alexander.konopko@gmail.com>
2014-12-10 12:15:44 +0200

    use private field utf codes

--- src/main/java/com/collective/celos/AugumentedHdfsPath.java
+++ src/main/java/com/collective/celos/AugumentedHdfsPath.java
@@ -1,30 +1,51 @@
 package com.collective.celos;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 /**
  * Created by akonopko on 09.12.14.
  */
 public class AugumentedHdfsPath {
 
-    private final URI uri;
+    private static Map<String, String> converstionBeforeParse = ImmutableMap.of(
+            "$", Character.toString((char) 0xE000),
+            "{", Character.toString((char) 0xE001),
+            "}", Character.toString((char) 0xE002));
 
-    public AugumentedHdfsPath(String hdfsPrefix, String original) {
-        uri = getUri(hdfsPrefix, URI.create(original));
-    }
 
-    private URI getUri(String hdfsPrefix, URI hdfsUri) {
-        try {
-            return new URI(hdfsUri.getScheme(), hdfsUri.getUserInfo(), hdfsUri.getHost(), hdfsUri.getPort(),
-                    hdfsPrefix + hdfsUri.getPath(), hdfsUri.getQuery(),
-                    hdfsUri.getFragment());
-        } catch (Exception e) {
-            return hdfsUri;
+    private static Map<String, String> converstionAfterParse = ImmutableMap.of(
+            Character.toString((char) 0xE000), "$",
+            Character.toString((char) 0xE001), "{",
+            Character.toString((char) 0xE002), "}");
+
+    private final String augumentedPath;
+
+    public AugumentedHdfsPath(String hdfsPrefix, String path) throws URISyntaxException {
+
+        for (String ch : converstionBeforeParse.keySet()) {
+            path = path.replace(ch.toString(), converstionBeforeParse.get(ch).toString());
         }
+        URI oldUri = URI.create(path);
+
+        String host = oldUri.getHost();
+        if (oldUri.getRawSchemeSpecificPart().startsWith("///") && host == null) {
+            host = "";
+        }
+
+        URI newUri = new URI(oldUri.getScheme(), oldUri.getUserInfo(), host, oldUri.getPort(), hdfsPrefix + oldUri.getPath(), oldUri.getQuery(), oldUri.getFragment());
+        path = newUri.toString();
+        for (String ch : converstionAfterParse.keySet()) {
+            path = path.replace(ch.toString(), converstionAfterParse.get(ch).toString());
+        }
+        augumentedPath = path;
     }
 
     public String getPath() {
-        return uri.toString();
+        return augumentedPath;
     }
 
 }
--- src/test/java/com/collective/celos/AugumentedHdfsPathTest.java
+++ src/test/java/com/collective/celos/AugumentedHdfsPathTest.java
@@ -3,17 +3,20 @@ package com.collective.celos;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.net.URISyntaxException;
+
 /**
  * Created by akonopko on 09.12.14.
  */
 public class AugumentedHdfsPathTest {
 
     @Test
-    public void testAugumentedHdfsPath() {
-        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "/originalPath").getPath(), "/myprefix/originalPath");
-        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs:/originalPath").getPath(), "hdfs:/myprefix/originalPath");
-        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs://nameservice/originalPath").getPath(), "hdfs://nameservice/myprefix/originalPath");
-        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs://nameservice:2345/originalPath").getPath(), "hdfs://nameservice:2345/myprefix/originalPath");
+    public void testAugumentedHdfsPath() throws URISyntaxException {
+        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "/originalPath/${var1}/path/${var2}").getPath(), "/myprefix/originalPath/${var1}/path/${var2}");
+        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs:/originalPath/${var1}/path/${var2}").getPath(), "hdfs:/myprefix/originalPath/${var1}/path/${var2}");
+        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs:///originalPath/${var1}/path/${var2}").getPath(), "hdfs:///myprefix/originalPath/${var1}/path/${var2}");
+        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs://nameservice/originalPath/${var1}/path/${var2}").getPath(), "hdfs://nameservice/myprefix/originalPath/${var1}/path/${var2}");
+        Assert.assertEquals(new AugumentedHdfsPath("/myprefix", "hdfs://nameservice:2345/originalPath/${var1}/path/${var2}").getPath(), "hdfs://nameservice:2345/myprefix/originalPath/${var1}/path/${var2}");
     }
 
 }
