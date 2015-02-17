77300335a26ad51428f5450f244baed00ed50a17
Manuel Simoni <msimoni@gmail.com>
2015-02-17 11:55:42 +0100

    Use ci prefix for all CCI API calls.

--- samples/wordcount_hive/src/test/celos-ci/test.js
+++ samples/wordcount_hive/src/test/celos-ci/test.js
@@ -1,22 +1,20 @@
-importPackage(Packages.com.collective.celos.ci.testing.fixtures.compare);
+var tableScript = ci.fixFile("CREATE TABLE wordcount PARTITIONED BY (year INT)" +
+                          "  ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'" +
+                          "  WITH SERDEPROPERTIES ('avro.schema.url'='${SANDBOX}/schema/wordcount.avsc')" +
+                          "  STORED as INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'" +
+                          "  OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'");
 
-var tableScript = fixFile("CREATE TABLE wordcount PARTITIONED BY (year INT)" +
-                        "  ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'" +
-                        "  WITH SERDEPROPERTIES ('avro.schema.url'='${SANDBOX}/schema/wordcount.avsc')" +
-                        "  STORED as INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'" +
-                        "  OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'");
+var resTableScript = ci.fixFile("CREATE TABLE result (word string, number int)");
 
-var resTableScript = fixFile("CREATE TABLE result (word string, number int)");
-
-addTestCase({
+ci.addTestCase({
     name: "Hive wordcount test case 1",
     sampleTimeStart: "2013-12-20T16:00Z",
     sampleTimeEnd: "2013-12-20T16:00Z",
     inputs: [
-        hdfsInput(fixDirFromResource("test-1/input/wordcount"), "input/wordcount"),
-        hdfsInput(fixDirFromResource("test-1/schema"), "/schema"),
-        hiveInput("celosdb", "wordcount", tableScript, fixTableFromResource("test-1/input/tsv/wordcount.tsv")),
-        hiveInput("celosdb", "result", resTableScript)
+        ci.hdfsInput(ci.fixDirFromResource("test-1/input/wordcount"), "input/wordcount"),
+        ci.hdfsInput(ci.fixDirFromResource("test-1/schema"), "/schema"),
+        ci.hiveInput("celosdb", "wordcount", tableScript, ci.fixTableFromResource("test-1/input/tsv/wordcount.tsv")),
+        ci.hiveInput("celosdb", "result", resTableScript)
     ],
     outputs: [
         ci.fixTableCompare(
