3b248dc145215487fb47101de23aa610e5a81c11
Manuel Simoni <msimoni@gmail.com>
2015-02-19 13:01:59 +0100

    Simplify Hive wordcount sample.

--- samples/wordcount_hive/src/test/celos-ci/test.js
+++ samples/wordcount_hive/src/test/celos-ci/test.js
@@ -1,4 +1,4 @@
-var tableScript = ci.fixFile("CREATE TABLE wordcount PARTITIONED BY (year INT)" +
+var tableScript = ci.fixFile("CREATE TABLE input PARTITIONED BY (year INT)" +
                           "  ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'" +
                           "  WITH SERDEPROPERTIES ('avro.schema.url'='${SANDBOX}/schema/wordcount.avsc')" +
                           "  STORED as INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'" +
@@ -11,14 +11,13 @@ ci.addTestCase({
     sampleTimeStart: "2013-12-20T16:00Z",
     sampleTimeEnd: "2013-12-20T16:00Z",
     inputs: [
-        ci.hdfsInput(ci.fixDirFromResource("test-1/input/wordcount"), "input/wordcount"),
         ci.hdfsInput(ci.fixDirFromResource("test-1/schema"), "/schema"),
-        ci.hiveInput("wordcountdb", "wordcount", tableScript, ci.fixTableFromResource("test-1/input/tsv/wordcount.tsv")),
+        ci.hiveInput("wordcountdb", "input", tableScript, ci.fixTableFromResource("test-1/input.tsv")),
         ci.hiveInput("wordcountdb", "result", resTableScript)
     ],
     outputs: [
         ci.fixTableCompare(
-            ci.fixTableFromResource("test-1/output/tsv/result.tsv"),
+            ci.fixTableFromResource("test-1/result.tsv"),
             ci.hiveTable("wordcountdb", "result")
         )
     ]
