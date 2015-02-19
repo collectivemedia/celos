47d5e4c7bec6e64c1bda65a85a261d41b984e072
Manuel Simoni <msimoni@gmail.com>
2015-02-19 12:27:41 +0100

    Use ci prefix for MR wordcount test.js

--- samples/wordcount/src/test/celos-ci/test.js
+++ samples/wordcount/src/test/celos-ci/test.js
@@ -1,11 +1,11 @@
-addTestCase({
+ci.addTestCase({
     name: "wordcount test case 1",
     sampleTimeStart: "2013-12-20T16:00Z",
     sampleTimeEnd: "2013-12-20T18:00Z",
     inputs: [
-        hdfsInput(fixDirFromResource("test-1/input"), "/input/wordcount")
+        ci.hdfsInput(ci.fixDirFromResource("test-1/input"), "/input/wordcount")
     ],
     outputs: [
-        plainCompare(fixDirFromResource("test-1/output"), "/output/wordcount")
+        ci.plainCompare(ci.fixDirFromResource("test-1/output"), "/output/wordcount")
     ]
 });
