a22f20547fc45a226e73f31bc99df9d70cdba710
Alexander Konopko <alexander.konopko@gmail.com>
2015-04-09 16:59:44 +0200

    return a list of Workflow Slots if no id was provided

--- celos-ci/src/test/java/com/collective/celos/ci/mode/test/client/CelosClientTest.java
+++ celos-ci/src/test/java/com/collective/celos/ci/mode/test/client/CelosClientTest.java
@@ -4,6 +4,7 @@ import com.collective.celos.ScheduledTime;
 import com.collective.celos.SlotID;
 import com.collective.celos.SlotState;
 import com.collective.celos.WorkflowID;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.UnknownHostException;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
@@ -35,36 +37,77 @@ public class CelosClientTest {
     @Test
     public void testParseWorkflowStatusesMap() throws IOException {
 
-        String str = "" +
-                "{\n" +
-                "\"info\": { \"url\": \"http://myurl\", \"contacts\": [ { \"name\": \"John Doe\", \"email\": \"John.Doe@Gmail.Com\"} ] },\n" +
-                "\"slots\": [\n" +
-                "  {\n" +
-                "    \"time\" : \"2014-10-27T14:00:00.000Z\",\n" +
-                "    \"status\" : \"SUCCESS\",\n" +
-                "    \"externalID\" : \"0029532-141007123109603-oozie-oozi-W\",\n" +
-                "    \"retryCount\" : 0\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"time\" : \"2014-10-27T15:00:00.000Z\",\n" +
-                "    \"status\" : \"FAILURE\",\n" +
-                "    \"externalID\" : \"0029595-141007123109603-oozie-oozi-W\",\n" +
-                "    \"retryCount\" : 2\n" +
-                "   }" +
+        String str =
+                "{ \"workflows\": [\n" +
+                "    {\n" +
+                "    \"id\": \"id1\",\n" +
+                "    \"info\": { \"url\": \"http://myurl\", \"contacts\": [ { \"name\": \"John Doe\", \"email\": \"John.Doe@Gmail.Com\"} ] },\n" +
+                "    \"slots\": [\n" +
+                "      {\n" +
+                "        \"time\" : \"2014-10-27T14:00:00.000Z\",\n" +
+                "        \"status\" : \"SUCCESS\",\n" +
+                "        \"externalID\" : \"0029532-141007123109603-oozie-oozi-W\",\n" +
+                "        \"retryCount\" : 0\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"time\" : \"2014-10-27T15:00:00.000Z\",\n" +
+                "        \"status\" : \"FAILURE\",\n" +
+                "        \"externalID\" : \"0029595-141007123109603-oozie-oozi-W\",\n" +
+                "        \"retryCount\" : 2\n" +
+                "       }" +
+                "       ]" +
+                "    }," +
+                "    {\n" +
+                "    \"id\": \"id2\",\n" +
+                "    \"info\": { \"url\": \"http://myurl\", \"contacts\": [ { \"name\": \"John Doe\", \"email\": \"John.Doe@Gmail.Com\"} ] },\n" +
+                "    \"slots\": [\n" +
+                "      {\n" +
+                "        \"time\" : \"2014-10-27T14:00:00.000Z\",\n" +
+                "        \"status\" : \"SUCCESS\",\n" +
+                "        \"externalID\" : \"0029532-141007123109603-oozie-oozi-W\",\n" +
+                "        \"retryCount\" : 0\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"time\" : \"2014-10-27T15:00:00.000Z\",\n" +
+                "        \"status\" : \"FAILURE\",\n" +
+                "        \"externalID\" : \"0029595-141007123109603-oozie-oozi-W\",\n" +
+                "        \"retryCount\" : 2\n" +
+                "       }" +
+                "       ]" +
+                "    }" +
                 "   ]" +
                 "}";
-
-        WorkflowID workflowID = new WorkflowID("123");
-        List<SlotState> result = new CelosClient("localhost").parseWorkflowStatusesMap(workflowID, new ByteArrayInputStream(str.getBytes())).getSlotStates();
+        WorkflowID workflowID1 = new WorkflowID("id1");
+        WorkflowID workflowID2 = new WorkflowID("id2");
+        Set<WorkflowStatus> result = new CelosClient("localhost").parseWorkflowStatusesMap(new ByteArrayInputStream(str.getBytes()));
         Assert.assertEquals(result.size(), 2);
 
         ScheduledTime time1 = new ScheduledTime("2014-10-27T14:00:00.000Z");
         ScheduledTime time2 = new ScheduledTime("2014-10-27T15:00:00.000Z");
 
-        SlotState val1 = new SlotState(new SlotID(workflowID, time1), SlotState.Status.SUCCESS, "0029532-141007123109603-oozie-oozi-W", 0);
-        SlotState val2 = new SlotState(new SlotID(workflowID, time2), SlotState.Status.FAILURE, "0029595-141007123109603-oozie-oozi-W", 2);
+        SlotState val1 = new SlotState(new SlotID(workflowID1, time1), SlotState.Status.SUCCESS, "0029532-141007123109603-oozie-oozi-W", 0);
+        SlotState val2 = new SlotState(new SlotID(workflowID1, time2), SlotState.Status.FAILURE, "0029595-141007123109603-oozie-oozi-W", 2);
 
-        Assert.assertEquals(Sets.newHashSet(result), Sets.newHashSet(val1, val2));
+        SlotState val3 = new SlotState(new SlotID(workflowID2, time1), SlotState.Status.SUCCESS, "0029532-141007123109603-oozie-oozi-W", 0);
+        SlotState val4 = new SlotState(new SlotID(workflowID2, time2), SlotState.Status.FAILURE, "0029595-141007123109603-oozie-oozi-W", 2);
+
+
+        List<WorkflowStatus> resultAsList = Lists.newArrayList(result);
+
+        resultAsList.sort(new Comparator<WorkflowStatus>() {
+            @Override
+            public int compare(WorkflowStatus o1, WorkflowStatus o2) {
+                return o1.getId().toString().compareTo(o2.getId().toString());
+            }
+        });
+
+        List<Set<SlotState>> expectedSlots = Lists.newArrayList();
+        expectedSlots.add(Sets.newHashSet(val1, val2));
+        expectedSlots.add(Sets.newHashSet(val3, val4));
+
+        for (int i = 0; i < resultAsList.size(); i++) {
+            Assert.assertEquals(Sets.newHashSet(resultAsList.get(i).getSlotStates()), expectedSlots.get(i));
+        }
     }
 
 
--- src/main/java/com/collective/celos/servlet/JSONWorkflowSlotsServlet.java
+++ src/main/java/com/collective/celos/servlet/JSONWorkflowSlotsServlet.java
@@ -2,6 +2,8 @@ package com.collective.celos.servlet;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,30 +14,49 @@ import com.collective.celos.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.Lists;
 
 /**
  * Returns information about the slot states of a single workflow as JSON.
- * 
- * GET /workflow-slots?id=workflow-1
+ *
+ * GET /workflow-slots
  * ==>
  * {
- *   "info": {
- *       "url": "http://myurl",
- *       "contacts": [
- *           { "name": "John Doe", "email": "John.Doe@Gmail.Com"},
- *           { "name": "Jack Smith", "email": "Jack.Smith@Gmail.Com"},
+ *     "workflows" : [
+ *     {
+ *       "id": "workflow-1",
+ *       "info": {
+ *           "url": "http://myurl",
+ *           "contacts": [
+ *               { "name": "John Doe", "email": "John.Doe@Gmail.Com"},
+ *               { "name": "Jack Smith", "email": "Jack.Smith@Gmail.Com"},
+ *           ]
+ *       },
+ *       "slots": [
+ *          { "time": "2013-12-07T13:00:00.000Z", "status": "RUNNING", "externalID": "237982137-371832798321-W", retryCount: 5 },
+ *          { "time": "2013-12-07T14:00:00.000Z", "status": "READY", "externalID": null, retryCount: 0 },
+ *         ...
  *       ]
- *   },
- *   "slots": [
- *      { "time": "2013-12-07T13:00:00.000Z", "status": "RUNNING", "externalID": "237982137-371832798321-W", retryCount: 5 },
- *      { "time": "2013-12-07T14:00:00.000Z", "status": "READY", "externalID": null, retryCount: 0 },
- *     ...
+ *     }, {
+ *       "id": "workflow-2",
+ *       "info": {
+ *           "url": "http://myurl2",
+ *           "contacts": [
+ *               { "name": "Ivan Ivanov", "email": "ivan.ivanov@Gmail.Com"},
+ *               { "name": "Ivan Petrov", "email": "ivan.petrov@Gmail.Com"},
+ *           ]
+ *       },
+ *       "slots": [
+ *          { "time": "2013-12-07T13:00:00.000Z", "status": "RUNNING", "externalID": "237982137-371832798322-W", retryCount: 2 },
+ *          { "time": "2013-12-07T14:00:00.000Z", "status": "READY", "externalID": null, retryCount: 0 },
+ *         ...
+ *       ]
+ *     }
  *   ]
  * }
- * 
- * If the "time" parameter is supplied, information is returned about 
+ * If the "time" parameter is supplied, information is returned about
  * slot states up to that time.
  */
 @SuppressWarnings("serial")
@@ -48,28 +69,49 @@ public class JSONWorkflowSlotsServlet extends AbstractJSONServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
         String id = req.getParameter(ID_PARAM);
         try {
-            if (id == null) {
-                res.sendError(HttpServletResponse.SC_BAD_REQUEST, ID_PARAM + " parameter missing.");
-                return;
-            }
             Scheduler scheduler = getOrCreateCachedScheduler();
-            Workflow wf = scheduler.getWorkflowConfiguration().findWorkflow(new WorkflowID(id));
-            if (wf == null) {
+            WorkflowConfiguration cfg = scheduler.getWorkflowConfiguration();
+
+            Collection<Workflow> workflows = getWorkflows(id, cfg);
+            if (workflows == null) {
                 res.sendError(HttpServletResponse.SC_NOT_FOUND, "Workflow not found: " + id);
             } else {
-                ObjectNode node = mapper.createObjectNode();
-                List<SlotState> slotStates = scheduler.getSlotStates(wf, getRequestTime(req));
-                List<JsonNode> objectNodes = Lists.newArrayList();
-                for (SlotState state : Lists.reverse(slotStates)) {
-                    objectNodes.add(state.toJSONNode());
+                ArrayNode list = mapper.createArrayNode();
+                ObjectNode object = mapper.createObjectNode();
+
+                for (Workflow wf : workflows) {
+                    ObjectNode node = mapper.createObjectNode();
+                    List<SlotState> slotStates = scheduler.getSlotStates(wf, getRequestTime(req));
+                    List<JsonNode> objectNodes = Lists.newArrayList();
+                    for (SlotState state : Lists.reverse(slotStates)) {
+                        objectNodes.add(state.toJSONNode());
+                    }
+                    node.put(ID_PARAM, wf.getID().toString());
+                    node.put(INFO_PARAM, mapper.valueToTree(wf.getWorkflowInfo()));
+                    node.putArray(SLOTS_PARAM).addAll(objectNodes);
+                    list.add(node);
                 }
-                node.put(INFO_PARAM, mapper.valueToTree(wf.getWorkflowInfo()));
-                node.putArray(SLOTS_PARAM).addAll(objectNodes);
-                writer.writeValue(res.getOutputStream(), node);
+                object.put("workflows", list);
+                writer.writeValue(res.getOutputStream(), object);
             }
         } catch (Exception e) {
             throw new ServletException(e);
         }
+    }
+
+    private Collection<Workflow> getWorkflows(String id, WorkflowConfiguration cfg) throws IOException {
+        Collection<Workflow> workflows;
+        if (id == null) {
+            workflows = cfg.getWorkflows();
+        } else {
+            Workflow wf = cfg.findWorkflow(new WorkflowID(id));
+            if (wf == null) {
+                workflows = null;
+            } else {
+                workflows = Arrays.asList(wf);
+            }
+        }
+        return workflows;
     }
 
 }
