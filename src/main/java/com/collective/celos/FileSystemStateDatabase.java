8cc0f9857bebb5b4ca37b1bb9b9b75bd593cce27
Oleg Baskakov <obaskakov@collective.com>
2015-07-22 11:35:41 -0400

    rerun db cleanup

--- src/main/java/com/collective/celos/FileSystemStateDatabase.java
+++ src/main/java/com/collective/celos/FileSystemStateDatabase.java
@@ -1,12 +1,18 @@
 package com.collective.celos;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Brutally simple persistent implementation of StateDatabase
@@ -40,20 +46,28 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  */
 public class FileSystemStateDatabase implements StateDatabase {
 
-    private static final String CHARSET = "UTF-8";
+    private static final ScheduledTimeFormatter FORMATTER = new ScheduledTimeFormatter();
+    private static final Logger LOGGER = Logger.getLogger(FileSystemStateDatabase.class);
 
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final ScheduledTimeFormatter formatter = new ScheduledTimeFormatter();
+
+    private static final String STATE_PATH  = "state";
+    private static final String RERUN_PATH = "rerun";
+
     private final File dir;
-    
+
     /**
      * Creates a new DB that stores data in the given directory, which must exist.
      */
     public FileSystemStateDatabase(File dir) throws IOException {
-        this.dir = Util.requireNonNull(dir);
-        if (!dir.exists()) {
-            throw new IOException("Database directory " + dir + " doesn't exist.");
-        }
+        assert dir != null;
+        this.dir = dir;
+        assert Files.isDirectory(dir.toPath()); // Database state directory must exists.
+        try {
+            Files.createDirectory(dir.toPath().resolve(RERUN_PATH));
+        } catch (FileAlreadyExistsException ignored) {}
+        try {
+            Files.createDirectory(dir.toPath().resolve(STATE_PATH));
+        } catch (FileAlreadyExistsException ignored) {}
     }
 
     @Override
@@ -62,18 +76,75 @@ public class FileSystemStateDatabase implements StateDatabase {
         if (!file.exists()) {
             return null;
         } else {
-            String json = FileUtils.readFileToString(file, CHARSET);
-            return SlotState.fromJSONNode(id, (ObjectNode) mapper.readTree(json));
+            JsonNode node = readJsonFromPath(file.toPath());
+            return SlotState.fromJSONNode(id, node);
         }
     }
 
     @Override
     public void putSlotState(SlotState state) throws Exception {
-        String json = mapper.writeValueAsString(state.toJSONNode());
-        File file = getSlotFile(state.getSlotID());
-        FileUtils.forceMkdir(file.getParentFile());
-        FileUtils.write(file, json, CHARSET);
+        final Path file = getSlotFile(state.getSlotID()).toPath();
+        writeJsonableToPath(state.toJSONNode(), file);
     }
+
+
+    private Path getRerunWorkflowPath(WorkflowID wfId) {
+        return dir.toPath().resolve(STATE_PATH).resolve(wfId.toString());
+    }
+
+
+    private Path getRerunSlotPath(SlotID slotId) {
+        final String dayRepr = FORMATTER.formatDatestamp(slotId.getScheduledTime());
+        final String timeRepr = FORMATTER.formatTimestamp(slotId.getScheduledTime());
+        final Path wfPath = getRerunWorkflowPath(slotId.getWorkflowID());
+        return wfPath.resolve(dayRepr).resolve(timeRepr);
+    }
+
+
+    private void addRerunSlotID(SlotID id, ScheduledTime current) throws Exception {
+        final RerunState rerunState = RerunState.fromTime(id.getWorkflowID(), id.getScheduledTime(), current);
+        final Path slotFile = getRerunSlotPath(id);
+        writeJsonableToPath(rerunState.toJsonNode(), slotFile);
+    }
+
+
+    @Override
+    public boolean updateSlotToRerun(SlotID slot, ScheduledTime current) throws Exception {
+        SlotState state = getSlotState(slot);
+        if (state == null) {
+            throw new IllegalStateException();
+        }
+        final SlotState slotState = state.transitionToRerun();
+        putSlotState(slotState);
+        addRerunSlotID(slotState.getSlotID(), current);
+        return true;
+    }
+
+    @Override
+    public List<SlotID> getRerunSlotIDs(WorkflowID wfId, ScheduledTime current) throws Exception {
+        final List<SlotID> res = new ArrayList<>();
+        final Path wfPath = getRerunWorkflowPath(wfId);
+        if (!Files.isDirectory(wfPath)) {
+            return res;
+        }
+        // else
+        final File[] daysDir = wfPath.toFile().listFiles();
+        assert daysDir != null;
+        for (File slotsDir : daysDir) {
+            final Path slotsDirPath = slotsDir.toPath();
+            assert Files.isDirectory(slotsDirPath);
+            File[] listSlotFiles = slotsDir.listFiles();
+            assert listSlotFiles != null;
+            for (File file : listSlotFiles) {
+                final JsonNode node = readJsonFromPath(file.toPath());
+                final RerunState st = RerunState.fromJsonNode(node);
+                final SlotID slotId = new SlotID(st.getWorkflowId(), st.getSlotTime());
+                res.add(slotId);
+            }
+        }
+        return res;
+    }
+
 
     private File getSlotFile(SlotID slotID) {
         return new File(getDayDir(slotID), getSlotFileName(slotID));
@@ -82,17 +153,40 @@ public class FileSystemStateDatabase implements StateDatabase {
     /** Returns the directory containing a day's data inside the workflow dir. */
     private File getDayDir(SlotID slotID) {
         File workflowDir = getWorkflowDir(slotID);
-        File dayDir = new File(workflowDir, formatter.formatDatestamp(slotID.getScheduledTime()));
+        File dayDir = new File(workflowDir, FORMATTER.formatDatestamp(slotID.getScheduledTime()));
         return dayDir;
     }
 
     /** Returns the directory containing all data for the slot's workflow. */
     private File getWorkflowDir(SlotID slotID) {
-        return new File(dir, slotID.getWorkflowID().toString());
+        return dir.toPath().resolve(STATE_PATH).resolve(slotID.getWorkflowID().toString()).toFile();
     }
     
     private String getSlotFileName(SlotID slotID) {
-        return formatter.formatTimestamp(slotID.getScheduledTime());
+        return FORMATTER.formatTimestamp(slotID.getScheduledTime());
+    }
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+
+    public static void writeJsonableToPath(JsonNode obj, Path path) throws Exception {
+        assert path != null;
+        Files.createDirectories(path.getParent());
+        final String json = MAPPER.writeValueAsString(obj);
+        Files.write(path, json.getBytes(CHARSET));
+    }
+
+    public static JsonNode readJsonFromPath(Path path) throws Exception {
+        assert path != null;
+        assert Files.isRegularFile(path);
+        String jsonData = new String(Files.readAllBytes(path), CHARSET);
+        return MAPPER.readTree(jsonData);
+    }
+
+    public static boolean isDirectoryEmpty(final Path directory) throws IOException {
+        try(DirectoryStream<Path> dirStream = Files.newDirectoryStream(directory)) {
+            return !dirStream.iterator().hasNext();
+        }
     }
 
 }
---
+++ src/main/java/com/collective/celos/RerunState.java
@@ -0,0 +1,59 @@
+package com.collective.celos;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class RerunState extends ValueObject {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    protected static final int GC_PERIOD_DAYS = 14;
+
+    private static final String RERUN_TIME_PROP = "rerunTime";
+    private static final String SLOT_TIME_PROP = "scheduledTime";
+    private static final String WORKFLOW_ID_PROP = "workflowId";
+
+    private final WorkflowID workflowId;
+    private final ScheduledTime slotTime;
+    private final ScheduledTime rerunTime;
+
+    public static RerunState fromJsonNode(JsonNode node) {
+        ScheduledTime slotTime = new ScheduledTime(node.get(SLOT_TIME_PROP).textValue());
+        ScheduledTime rerunTime = new ScheduledTime(node.get(RERUN_TIME_PROP).textValue());
+        WorkflowID wfId = new WorkflowID(node.get(WORKFLOW_ID_PROP).textValue());
+        return new RerunState(wfId, slotTime, rerunTime);
+    }
+
+    public JsonNode toJsonNode() {
+        ObjectNode node = MAPPER.createObjectNode();
+        node.put(RERUN_TIME_PROP, rerunTime.toString());
+        node.put(WORKFLOW_ID_PROP, workflowId.toString());
+        node.put(SLOT_TIME_PROP, slotTime.toString());
+        return node;
+    }
+
+    public static RerunState fromTime(WorkflowID wfId, ScheduledTime slotTime, ScheduledTime rerunTime) {
+        return new RerunState(wfId, slotTime, rerunTime);
+    }
+
+    private RerunState(WorkflowID wfId, ScheduledTime slotTime, ScheduledTime rerunTime) {
+        assert wfId != null;
+        this.workflowId = wfId;
+        assert slotTime != null;
+        this.slotTime = slotTime;
+        assert rerunTime != null;
+        this.rerunTime = rerunTime;
+    }
+
+    public WorkflowID getWorkflowId() {
+        return workflowId;
+    }
+
+    public ScheduledTime getSlotTime() {
+        return slotTime;
+    }
+
+    public ScheduledTime getRerunTime() {
+        return rerunTime;
+    }
+}
