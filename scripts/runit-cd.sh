cad0c287992c9c1d113803fe2c479505bfc62087
Oleg Baskakov <obaskakov@collective.com>
2015-07-30 18:47:01 -0400

    Check jenkins Project celos-runit.test. DEST_ROOT rename.

--- scripts/runit-cd.sh
+++ scripts/runit-cd.sh
@@ -3,18 +3,19 @@ set -x
 set -e
 
 [[ -z ${SERVICE_NAME} ]] && exit 1
+[[ -z ${SERVICE_USER} ]] && exit 1
 
 DEPLOY_HOST=admin1.lga2.collective-media.net
 JAR_FILE="${SERVICE_NAME}-0.1.jar"
 JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
-SERVICE_USER="celos-ci"
-DEST_ROOT="/home/celos-ci/local"
+DEST_ROOT="/home/${SERVICE_USER}/local"
 
 ./gradlew clean test "${SERVICE_NAME}:jar"
 
 mkdir -p ${DEST_ROOT}/bin
 echo >  ${DEST_ROOT}/bin/${SERVICE_NAME} "#!/usr/bin/env bash"
-echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec chpst -u ${SERVICE_USER} ${DEST_ROOT}/lib/${JAR_FILE}"
+echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec java -jar ${DEST_ROOT}/lib/${JAR_FILE} --port 8080 --celosAddr http://localhost"
+chmod a+x ${DEST_ROOT}/bin/${SERVICE_NAME}
 
 mkdir -p ${DEST_ROOT}
 cp -f ${JAR_PATH} ${DEST_ROOT}/lib/
@@ -25,5 +26,5 @@ cp -f ${JAR_PATH} ${DEST_ROOT}/lib/
 #ansible all -i "${DEPLOY_HOST}," -u "${SERVICE_USER}" -m copy -a "src=${JAR_PATH} dest=${DEST_PATH}"
 #ansible all -i "${DEPLOY_HOST}," -u "${SERVICE_USER}" -m command -a "sv restart ${SERVICE_NAME}"
 
-# sleep 0.5
-#curl http://localhost:8080 | diff - test.tmp
+sleep 2.5
+curl "http://${DEPLOY_HOST}:8080/ui" | diff - scripts/hello-ui.txt
