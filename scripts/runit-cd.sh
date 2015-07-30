93f30fab83bb51b236c0da84f15e310653553d58
Oleg Baskakov <obaskakov@collective.com>
2015-07-30 17:53:29 -0400

    Check jenkins Project celos-runit.test. Next try.

--- scripts/runit-cd.sh
+++ scripts/runit-cd.sh
@@ -5,15 +5,21 @@ set -e
 [[ -z ${SERVICE_NAME} ]] && exit 1
 
 DEPLOY_HOST=admin1.lga2.collective-media.net
-JAR_PATH="./${SERVICE_NAME}/build/libs/${SERVICE_NAME}-0.1.jar"
+JAR_FILE="${SERVICE_NAME}-0.1.jar"
+JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
 SERVICE_USER="celos-ci"
-DEST_PATH="/home/celos-ci/local/lib/"
+DEST_ROOT="/home/celos-ci/local"
 
 ./gradlew clean test "${SERVICE_NAME}:jar"
 
+mkdir -p ${DEST_ROOT}/bin
+echo >  ${DEST_ROOT}/bin/${SERVICE_NAME} "#!/usr/bin/env bash"
+echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec chpst -u ${SERVICE_USER} ${DEST_ROOT}/lib/${JAR_FILE}"
+
 mkdir -p ${DEST_PATH}
-cp -f ${JAR_PATH} ${DEST_PATH}
+cp -f ${JAR_PATH} ${DEST_ROOT}/lib/
 /sbin/sv restart ${SERVICE_NAME}
+
 
 #ansible all -i "${DEPLOY_HOST}," -u "${SERVICE_USER}" -m command -a "mkdir -p ${DEST_PATH}"
 #ansible all -i "${DEPLOY_HOST}," -u "${SERVICE_USER}" -m copy -a "src=${JAR_PATH} dest=${DEST_PATH}"
