5faf31c25c07acad0b0f4f92ed5879533cd0b085
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-03 16:05:04 +0300

    add Util.requireNonNull

--- celos-ci/src/main/java/com/collective/celos/ci/config/CiCommandLine.java
+++ celos-ci/src/main/java/com/collective/celos/ci/config/CiCommandLine.java
@@ -1,5 +1,6 @@
 package com.collective.celos.ci.config;
 
+import com.collective.celos.Util;
 import com.collective.celos.ci.config.deploy.CelosCiContext;
 
 import java.io.File;
@@ -19,13 +20,13 @@ public class CiCommandLine {
     private final boolean keepTempData;
 
     public CiCommandLine(String targetUri, String mode, String deployDir, String workflowName, String testCasesDir, String userName, boolean keepTempData) {
-        this.userName = userName;
+        this.userName = Util.requireNonNull(userName);
         this.keepTempData = keepTempData;
-        this.targetUri = URI.create(targetUri);
-        this.mode = CelosCiContext.Mode.valueOf(mode);
-        this.deployDir = new File(deployDir);
-        this.workflowName = workflowName;
-        this.testCasesDir = new File(testCasesDir);
+        this.targetUri = URI.create(Util.requireNonNull(targetUri));
+        this.mode = CelosCiContext.Mode.valueOf(Util.requireNonNull(mode));
+        this.deployDir = new File(Util.requireNonNull(deployDir));
+        this.workflowName = Util.requireNonNull(workflowName);
+        this.testCasesDir = new File(Util.requireNonNull(testCasesDir));
     }
 
     public URI getTargetUri() {
