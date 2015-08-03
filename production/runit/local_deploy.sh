f431d1160452d6131f65bd88e776e0aa3b206612
Oleg Baskakov <obaskakov@collective.com>
2015-08-03 18:48:32 -0400

    warning fix

--- production/runit/local_deploy.sh
+++ production/runit/local_deploy.sh
@@ -4,13 +4,11 @@ set -e
 
 [[ -z ${SERVICE_NAME} ]] && echo pls set SERVICE_NAME && exit 1
 [[ -z ${SERVICE_USER} ]] && echo pls set SERVICE_USER && exit 1
-
-DEPLOY_HOST=localhost
-DEPLOY_PORT=8080
+[[ -z ${DEPLOY_PORT} ]] && echo pls set DEPLOY_PORT && exit 1
 
 SV="/sbin/sv"
 
-SERVICE_HOME="/home/${SERVICE_USER}"
+SERVICE_HOME="$(eval echo ~${SERVICE_USER})"
 
 if [[ ! -d ${SERVICE_HOME} ]]
 then
@@ -25,19 +23,21 @@ SERVICE_DIR="${SV_PATH}/${SERVICE_NAME}"
 JAR_FILE="${SERVICE_NAME}-0.1.jar"
 JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
 
-
 DEST_ROOT="${SERVICE_HOME}/local"
 RUN_SCRIPT="${DEST_ROOT}/bin/${SERVICE_NAME}"
 
 mkdir -p ${SV_PATH}
-mkdir -p ${SERVICE_DIR}/log
 # process supervise
 mkdir -p ${SERVICE_DIR}/supervise
 chmod 755 ${SERVICE_DIR}/supervise
-
-
 [ -p ${SERVICE_DIR}/supervise/ok ]      || mkfifo ${SERVICE_DIR}/supervise/ok
 [ -p ${SERVICE_DIR}/supervise/control ] || mkfifo ${SERVICE_DIR}/supervise/control
+# logs supervise
+mkdir -p ${SERVICE_DIR}/log
+mkdir -p ${SERVICE_DIR}/log/supervise
+chmod 755 ${SERVICE_DIR}/log/supervise
+[ -p ${SERVICE_DIR}/log/supervise/ok ]      || mkfifo ${SERVICE_DIR}/log/supervise/ok
+[ -p ${SERVICE_DIR}/log/supervise/control ] || mkfifo ${SERVICE_DIR}/log/supervise/control
 
 # process run script
 mkdir -p ${SERVICE_DIR}
@@ -58,12 +58,8 @@ mkdir -p ${DEST_ROOT}/bin
 echo >  ${DEST_ROOT}/bin/${SERVICE_NAME} '#!/usr/bin/env bash'
 echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec java -jar ${DEST_ROOT}/lib/${JAR_FILE} --port ${DEPLOY_PORT} --celosAddr http://localhost"
 chmod a+x ${DEST_ROOT}/bin/${SERVICE_NAME}
-# process jar
-./gradlew ${SERVICE_NAME}:jar
-mkdir -p ${DEST_ROOT}/lib
-cp -f ${JAR_PATH} ${DEST_ROOT}/lib/
 # restart service
-if [ ${SV} check ${SERVICE_NAME} ]
+if ${SV} check ${SERVICE_NAME} ;
 then
     ln -sf ${SERVICE_DIR} /etc/service/${SERVICE_NAME}
     ${SV} restart ${SERVICE_NAME}
@@ -74,4 +70,3 @@ else
     while (( i <= 7 )) && ! ${SV} check ${SERVICE_NAME} ; do (( i += 1 )) ; sleep 1 ; done
 fi
 
-curl "http://${DEPLOY_HOST}:${DEPLOY_PORT}/ui" 2> /dev/null | diff - production/runit/hello-ui.txt
---
+++ production/runit/remote_deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+[[ -z ${SERVICE_NAME} ]] && echo pls set SERVICE_NAME && exit 1
+[[ -z ${SERVICE_USER} ]] && echo pls set SERVICE_USER && exit 1
+[[ -z ${DEPLOY_HOST} ]] && echo pls set DEPLOY_HOST && exit 1
+[[ -z ${DEPLOY_PORT} ]] && echo pls set DEPLOY_PORT && exit 1
+
+
+SSH_NODE="${SERVICE_USER}@${DEPLOY_HOST}"
+LOCAL_SCRIPT="local_deploy.sh"
+
+JAR_FILE="${SERVICE_NAME}-0.1.jar"
+JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
+
+SERVICE_HOME="$(ssh ${SSH_NODE} echo ~${SERVICE_USER})"
+DEST_ROOT="${SERVICE_HOME}/local"
+
+# process jar
+#./gradlew ${SERVICE_NAME}:jar
+ssh ${SSH_NODE} "mkdir -p ${DEST_ROOT}/lib"
+scp "${JAR_PATH}" "${SSH_NODE}:${DEST_ROOT}/lib/"
+scp "./production/runit/${LOCAL_SCRIPT}" "${SSH_NODE}:${DEST_ROOT}/"
+# run local script
+ssh ${SSH_NODE} "DEPLOY_PORT=$DEPLOY_PORT SERVICE_NAME=$SERVICE_NAME SERVICE_USER=$SERVICE_USER\
+                 bash ${DEST_ROOT}/${LOCAL_SCRIPT}"
+
+
+
+curl "http://${DEPLOY_HOST}:${DEPLOY_PORT}/ui" 2> /dev/null | diff - production/runit/hello-ui.txt
