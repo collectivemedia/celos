5777e79b1ce859e8a8e378d14b4c443de87c71fc
Oleg Baskakov <obaskakov@collective.com>
2015-08-04 15:48:01 -0400

    celos-server support

--- production/runit/local_deploy.sh
+++ production/runit/local_deploy.sh
@@ -7,7 +7,7 @@ set -e
 [[ -z ${DEPLOY_HOST} ]] && echo pls set DEPLOY_HOST && exit 1
 [[ -z ${DEPLOY_PORT} ]] && echo pls set DEPLOY_PORT && exit 1
 
-SV="/usr/bin/sv"
+SV="/sbin/sv"
 
 SERVICE_HOME="$(eval echo ~${SERVICE_USER})"
 
@@ -21,7 +21,14 @@ SV_PATH0="runit-sv"
 SV_PATH="${SERVICE_HOME}/${SV_PATH0}"
 SERVICE_DIR="${SV_PATH}/${SERVICE_NAME}"
 
+# FIXME dirty hack
+if [[ $SERVICE_NAME == "celos-server" ]]
+then
+JAR_FILE="celos-0.1.jar"
+else
 JAR_FILE="${SERVICE_NAME}-0.1.jar"
+fi
+
 JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
 
 DEST_ROOT="${SERVICE_HOME}/local"
@@ -47,7 +54,7 @@ echo >>  ${SERVICE_DIR}/run "exec chpst -u ${SERVICE_USER} ${RUN_SCRIPT}"
 chmod a+x ${SERVICE_DIR}/run
 # process check script
 echo >  ${SERVICE_DIR}/check '#!/usr/bin/env bash'
-echo >>  ${SERVICE_DIR}/check "exec curl \"http://${DEPLOY_HOST}:${DEPLOY_PORT}/ui\" &> /dev/null"
+echo >>  ${SERVICE_DIR}/check "exec curl \"http://${DEPLOY_HOST}:${DEPLOY_PORT}\" &> /dev/null"
 chmod a+x ${SERVICE_DIR}/check
 # process logs
 mkdir -p ${SERVICE_DIR}/log
@@ -57,15 +64,23 @@ chmod a+x ${SERVICE_DIR}/log/run
 # process programm
 mkdir -p ${DEST_ROOT}/bin
 echo >  ${DEST_ROOT}/bin/${SERVICE_NAME} '#!/usr/bin/env bash'
+
+# FIXME dirty hack
+if [[ $SERVICE_NAME == "celos-server" ]]
+then
+echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec java -jar ${DEST_ROOT}/lib/${JAR_FILE} --port ${DEPLOY_PORT}"
+else
 echo >> ${DEST_ROOT}/bin/${SERVICE_NAME} "exec java -jar ${DEST_ROOT}/lib/${JAR_FILE} --port ${DEPLOY_PORT} --celosAddr http://localhost"
+fi
+
 chmod a+x ${DEST_ROOT}/bin/${SERVICE_NAME}
 # restart service
-if ${SV} check ${SERVICE_NAME} ;
+if ${SV} check ${SERVICE_NAME} 2> /dev/null ;
 then
     ln -sf ${SERVICE_DIR} /etc/service/${SERVICE_NAME}
     ${SV} restart ${SERVICE_NAME}
 else
-    # runsv starts service with delay, so this fixes 'fail: $SERVICE_NAME: runsv not running'
+    # runsv starts new service with delay, so this fixes 'fail: $SERVICE_NAME: runsv not running'
     ln -sf ${SERVICE_DIR} /etc/service/${SERVICE_NAME}
     i=0
     while (( i <= 7 )) && ! ${SV} check ${SERVICE_NAME} ; do (( i += 1 )) ; sleep 1 ; done
--- production/runit/remote_deploy.sh
+++ production/runit/remote_deploy.sh
@@ -11,14 +11,21 @@ set -e
 SSH_NODE="${SERVICE_USER}@${DEPLOY_HOST}"
 LOCAL_SCRIPT="./production/runit/local_deploy.sh"
 
+# FIXME dirty hack
+if [[ $SERVICE_NAME == "celos-server" ]]
+then
+JAR_FILE="celos-0.1.jar"
+else
 JAR_FILE="${SERVICE_NAME}-0.1.jar"
+fi
+
 JAR_PATH="./${SERVICE_NAME}/build/libs/${JAR_FILE}"
 
 SERVICE_HOME="$(ssh ${SSH_NODE} exec echo ~${SERVICE_USER})"
 DEST_ROOT="${SERVICE_HOME}/local"
 
 # process jar
-./gradlew clean ${SERVICE_NAME}:jar
+./gradlew ${SERVICE_NAME}:jar
 ssh ${SSH_NODE} "mkdir -p ${DEST_ROOT}/lib"
 scp "${JAR_PATH}" "${SSH_NODE}:${DEST_ROOT}/lib/"
 #scp "./production/runit/${LOCAL_SCRIPT}" "${SSH_NODE}:${DEST_ROOT}/"
@@ -27,4 +34,4 @@ ssh ${SSH_NODE} "DEPLOY_HOST=$DEPLOY_HOST DEPLOY_PORT=$DEPLOY_PORT \
                  SERVICE_NAME=$SERVICE_NAME SERVICE_USER=$SERVICE_USER \
                  bash -s" < ${LOCAL_SCRIPT}
 
-curl "http://${DEPLOY_HOST}:${DEPLOY_PORT}/ui" 2> /dev/null | diff - production/runit/hello-ui.txt
+# curl "http://${DEPLOY_HOST}:${DEPLOY_PORT}/ui" 2> /dev/null | diff - production/runit/hello-ui.txt
