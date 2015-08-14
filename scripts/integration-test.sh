ab4d04d6ce4190ec181a017028a12d9f5c8f784d
Oleg Baskakov <obaskakov@collective.com>
2015-08-14 15:57:37 -0400

    integration jenkins script added

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash -xe
-
+#!/usr/bin/env bash
+set -x
+set -e
 GIT_COMMIT=${GIT_COMMIT:-undefined}
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/celos-deploy.yaml -e version=${GIT_COMMIT}
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/test/celos_test.yaml -e version=${GIT_COMMIT}
