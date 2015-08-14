131fd987d6d4dca3c406de18277d6dcbad180b63
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-14 04:45:54 +0300

    restore origin file-copy

--- samples/file-copy/celos_deploy/workflow.js
+++ samples/file-copy/celos_deploy/workflow.js
@@ -5,11 +5,11 @@ addWorkflow({
     "maxRetryCount": 0,
     "schedule": hourlySchedule(),
     "schedulingStrategy": serialSchedulingStrategy(),
-    "trigger": hdfsCheckTrigger("/user/akonopko/celos.selftest/input/file-copy/${year}-${month}-${day}T${hour}00.txt"),
+    "trigger": hdfsCheckTrigger(hdfsPath("/test/input/file-copy/${year}-${month}-${day}T${hour}00.txt")),
     "externalService": oozieExternalService({
-        "oozie.wf.application.path": "/user/akonopko/celos.selftest/user/celos/app/file-copy/workflow.xml",
-        "inputDir": "hdfs:/user/akonopko/celos.selftest/input/file-copy",
-        "outputDir": "hdfs:/user/akonopko/celos.selftest/output/file-copy"
+        "oozie.wf.application.path": hdfsPath("/user/celos/app/file-copy/workflow.xml"),
+        "inputDir": hdfsPath("hdfs:/test/input/file-copy"),
+        "outputDir": hdfsPath("hdfs:/test/output/file-copy")
     })
 
 });
