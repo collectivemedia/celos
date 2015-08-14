c3309b9c777ac2f32c58d34c5430a6cb2ad36a4b
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-14 05:44:07 +0300

    adjustments

--- samples/file-copy-celos-provided/src/main/celos/workflow.js
+++ samples/file-copy-celos-provided/src/main/celos/workflow.js
@@ -5,11 +5,11 @@ addWorkflow({
     "maxRetryCount": 0,
     "schedule": hourlySchedule(),
     "schedulingStrategy": serialSchedulingStrategy(),
-    "trigger": hdfsCheckTrigger("/user/akonopko/celos.selftest/input/file-copy/${year}-${month}-${day}T${hour}00.txt"),
+    "trigger": hdfsCheckTrigger("/user/celos-ci/celos.selftest/input/file-copy/${year}-${month}-${day}T${hour}00.txt"),
     "externalService": oozieExternalService({
-        "oozie.wf.application.path": "/user/akonopko/celos.selftest/user/celos/app/file-copy/workflow.xml",
-        "inputDir": "hdfs:/user/akonopko/celos.selftest/input/file-copy",
-        "outputDir": "hdfs:/user/akonopko/celos.selftest/output/file-copy"
+        "oozie.wf.application.path": "/user/celos-ci/celos.selftest/user/celos/app/file-copy/workflow.xml",
+        "inputDir": "hdfs:/user/celos-ci/celos.selftest/input/file-copy",
+        "outputDir": "hdfs:/user/celos-ci/celos.selftest/output/file-copy"
     })
 
 });
