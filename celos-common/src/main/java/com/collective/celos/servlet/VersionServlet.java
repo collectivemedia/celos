9c52c086eb806c727c0d838bec345eb61d9c778a
Oleg Baskakov <obaskakov@collective.com>
2015-08-17 12:09:37 -0400

    VersionServlet.java moved to celos-common

--- celos-common/src/main/java/com/collective/celos/servlet/VersionServlet.java
+++ celos-common/src/main/java/com/collective/celos/servlet/VersionServlet.java
@@ -1,11 +1,12 @@
 package com.collective.celos.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-public class VersionServlet extends AbstractJSONServlet {
+public class VersionServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
         try {
@@ -13,7 +14,7 @@ public class VersionServlet extends AbstractJSONServlet {
             if (serviceVersion != null) {
                 res.getOutputStream().write(serviceVersion.getBytes());
             } else {
-                res.getOutputStream().write("undefined".getBytes());
+                throw new Exception("CELOS_VERSION is undefined");
             }
         } catch (Exception e) {
             throw new ServletException(e);
