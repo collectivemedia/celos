5572036dc5393ac3a7726ff687fdf8c6ed881f16
Oleg Baskakov <obaskakov@collective.com>
2015-08-17 11:58:56 -0400

    code cleanup

--- celos-ui/src/main/java/com/collective/celos/ui/VersionServlet.java
+++ celos-ui/src/main/java/com/collective/celos/ui/VersionServlet.java
@@ -10,7 +10,12 @@ public class VersionServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
         try {
-            res.getOutputStream().write(System.getenv("CELOS_VERSION").getBytes());
+            final String serviceVersion = System.getenv("CELOS_VERSION");
+            if (serviceVersion != null) {
+                res.getOutputStream().write(serviceVersion.getBytes());
+            } else {
+                res.getOutputStream().write("undefined".getBytes());
+            }
         } catch (Exception e) {
             throw new ServletException(e);
         }
--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 set -e
-GIT_COMMIT=${GIT_COMMIT:-undefined}
+[[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/celos-deploy.yaml -e version=${GIT_COMMIT}
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/test/celos_test.yaml -e version=${GIT_COMMIT}
