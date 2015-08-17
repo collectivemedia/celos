ccc6eafb5185e35f11213ca91be8ddce92d897e1
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-18 00:21:42 +0300

    try to run on oj001

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -2,5 +2,5 @@
 set -x
 set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
-ansible-playbook -i scripts/test/inventory -u obaskakov scripts/celos-deploy.yaml -e version=${GIT_COMMIT}
-ansible-playbook -i scripts/test/inventory -u obaskakov scripts/test/celos_test.yaml -e version=${GIT_COMMIT}
+ansible-playbook -i scripts/test/inventory -u celos-ci scripts/celos-deploy.yaml -e version=${GIT_COMMIT}
+ansible-playbook -i scripts/test/inventory -u celos-ci scripts/test/celos_test.yaml -e version=${GIT_COMMIT}
