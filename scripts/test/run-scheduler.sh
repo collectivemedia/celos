8b577dab2476b368419ae733a0323e65c408d627
Manuel Simoni <msimoni@gmail.com>
2015-08-17 19:51:17 +0200

    Revive old int test

---
+++ scripts/test/run-scheduler.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+set -x
+
+# Script for integration testing - runs scheduler until no slots
+# are READY or RUNNING, i.e. all slots have run to completion.
+
+SERVICE_URL=$1
+TIME=$2
+WORKFLOW=$3
+
+schedule() {
+    curl --fail -X POST "${SERVICE_URL}/scheduler?time=$TIME"
+}
+
+running() {
+    curl --fail "${SERVICE_URL}/workflow?time=$TIME&id=$WORKFLOW" | grep "READY\|RUNNING"
+}
+
+schedule
+while running
+do
+    schedule
+    sleep 1
+done
