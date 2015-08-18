56885a75daa2992681398451bdaa315a316d6c35
Oleg Baskakov <obaskakov@collective.com>
2015-08-18 14:44:34 -0400

    user params splited too, celos-cluster to host changed

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
-ansible-playbook scripts/celos-deploy.yaml -i scripts/conf/inventory-testing-server -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
-ansible-playbook scripts/celos-deploy.yaml -i scripts/conf/inventory-testing-ui     -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json"
-ansible-playbook scripts/test/celos_test.yaml -i scripts/conf/inventory-testing-server -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
+ansible-playbook scripts/celos-deploy.yaml -i scripts/conf/inventory-testing-server -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params-server.json" -e "@scripts/conf/common-params-server.json"
+ansible-playbook scripts/celos-deploy.yaml -i scripts/conf/inventory-testing-ui     -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params-ui.json" -e "@scripts/conf/common-params-ui.json"
+ansible-playbook scripts/test/celos_test.yaml -i scripts/conf/inventory-testing-server -e service_version=${GIT_COMMIT} -e "@scripts/conf/celos-params-server.json" -e "@scripts/conf/common-params-server.json"
