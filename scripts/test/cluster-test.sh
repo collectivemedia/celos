5e2e89b5db77bc6b383d843496252b179330dcfc
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-18 04:04:53 +0300

    back to prod mode

--- scripts/test/cluster-test.sh
+++ scripts/test/cluster-test.sh
@@ -4,7 +4,7 @@ set -x
 
 # Run integration tests against currently checkout local source on test cluster.
 
-# ansible-playbook -i inventory celos_dirs.yaml
+ansible-playbook -i inventory celos_dirs.yaml
 ansible-playbook -i inventory celos_test.yaml
 
 echo You win! All tests OK.
