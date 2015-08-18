a8a79e428b3849ee54f2b4059d9bf3173892b5e3
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-18 19:00:03 +0300

    remove celos_dirs

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -2,6 +2,5 @@
 set -x
 set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
-ansible-playbook -i scripts/test/inventory -u celos-ci scripts/test/celos_dirs.yaml
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/celos-deploy.yaml -e version=${GIT_COMMIT}
 ansible-playbook -i scripts/test/inventory -u celos-ci scripts/test/celos_test.yaml -e version=${GIT_COMMIT}
