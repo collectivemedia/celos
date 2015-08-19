9dadf885376332c074bc61931b1dcb85ce5b8bb6
Oleg Baskakov <obaskakov@collective.com>
2015-08-19 14:24:17 -0400

    ansible bug -e fix

--- scripts/prod/stop.sh
+++ scripts/prod/stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 set -e
-ansible all -m shell -a "/sbin/sv stop {{service_name}}" -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json"
-ansible all -m shell -a "/sbin/sv stop {{service_name}}" -i scripts/prod/conf/inventory-ui -e "@scripts/prod/conf/celos-params.json"
+ansible-playbook ./scripts/prod/celos-stop.yaml -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json"
+ansible-playbook ./scripts/prod/celos-stop.yaml -i scripts/prod/conf/inventory-ui -e "@scripts/prod/conf/celos-params.json"
