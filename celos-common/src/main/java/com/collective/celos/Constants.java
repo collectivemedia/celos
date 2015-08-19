2d590af2cbe8d59e0d416674aefa0210c4b77786
Alexander Konopko <alexander.konopko@gmail.com>
2015-08-19 17:13:16 +0300

    remove UI from celos-server

--- celos-common/src/main/java/com/collective/celos/Constants.java
+++ celos-common/src/main/java/com/collective/celos/Constants.java
@@ -14,7 +14,6 @@ public class Constants {
     public static final String WORKFLOW_CONFIGURATION_PATH_ATTR = "workflow.configuration.path";
     public static final String DEFAULTS_CONFIGURATION_PATH_ATTR = "defaults.configuration.path";
     public static final String STATE_DATABASE_PATH_ATTR = "state.database.path";
-    public static final String UI_PATH_ATTR = "ui.configuration.path";
     public static final String ADDITIONAL_JS_VARIABLES = "additional.js.variables";
 
 }
