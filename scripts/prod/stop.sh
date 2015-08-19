3cb7ea7e563824978e4a8af051bea4fd5e6e69e1
Oleg Baskakov <obaskakov@collective.com>
2015-08-19 14:27:42 -0400

    -u celos

--- scripts/prod/stop.sh
+++ scripts/prod/stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 set -e
-ansible-playbook ./scripts/prod/celos-stop.yaml -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json"
-ansible-playbook ./scripts/prod/celos-stop.yaml -i scripts/prod/conf/inventory-ui -e "@scripts/prod/conf/celos-params.json"
+ansible-playbook ./scripts/prod/celos-stop.yaml -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json"
+ansible-playbook ./scripts/prod/celos-stop.yaml -u celos -i scripts/prod/conf/inventory-ui -e "@scripts/prod/conf/celos-params.json"
