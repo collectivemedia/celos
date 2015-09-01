d3788d385a5c0ef6d876893c6d0bc435bbeb8a5c
Manuel Simoni <msimoni@gmail.com>
2015-09-01 12:33:09 +0200

    Remove test classes

--- scripts/push-javadoc.sh
+++ scripts/push-javadoc.sh
@@ -3,8 +3,8 @@
 # get sources path
 DIR="$(dirname ${BASH_SOURCE[0]})/.."
 INDEXES="com.collective.celos.ci.config.deploy com.collective.celos.ci.testing.structure.fixobject com.collective.celos.ui com.collective.celos.ci.testing.fixtures.deploy com.collective.celos.ci.testing.structure.tree com.collective.celos.ci.mode.test com.collective.celos.ci.config com.collective.celos.trigger com.collective.celos com.collective.celos.ci.mode com.collective.celos.ci.testing.fixtures.create com.collective.celos.ci.deploy com.collective.celos.ci com.collective.celos.ci.testing.fixtures.compare.json com.collective.celos.ci.testing.tree com.collective.celos.ci.testing.fixtures.deploy.hive com.collective.celos.server com.collective.celos.ci.testing.fixtures.compare com.collective.celos.ci.testing.fixtures.convert com.collective.celos.servlet"
-CLASSES="${DIR}/celos-server/build/classes/test:${DIR}/celos-server/build/classes/main:${DIR}/celos-server/build/resources/test:${DIR}/celos-server/build/resources/main"
-SOURCES="${DIR}/celos-common/src/main/java:${DIR}/celos-ui/src/main/java:${DIR}/celos-ui/src/test/java:${DIR}/celos-ui/src/main/webapp:${DIR}/celos-server/src/main/java:${DIR}/celos-server/src/test/java:${DIR}/celos-server/src/main/resources:${DIR}/celos-server/src/main/webapp:${DIR}/celos-server/src/test/resources:${DIR}/celos-ci/src/main/java:${DIR}/celos-ci/src/test/java:${DIR}/celos-ci/src/main/resources:${DIR}/celos-ci/src/test/resources"
+CLASSES="${DIR}/celos-server/build/classes/main:${DIR}/celos-server/build/resources/main"
+SOURCES="${DIR}/celos-common/src/main/java:${DIR}/celos-ui/src/main/java:${DIR}/celos-ui/src/main/webapp:${DIR}/celos-server/src/main/java:${DIR}/celos-server/src/main/resources:${DIR}/celos-server/src/main/webapp:${DIR}/celos-ci/src/main/java:${DIR}/celos-ci/src/main/resources"
 JAVADOC=${JAVA_HOME:-"/usr/java/latest"}/bin/javadoc
 
 set -e
