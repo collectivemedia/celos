e93d7b061e7a69d79147964ab1d65eee0b64a058
Oleg Baskakov <obaskakov@collective.com>
2015-09-01 13:41:31 -0400

    merge error CelosServer.java #438

--- celos-ci/src/main/java/com/collective/celos/ci/mode/test/TestRunCelosServerModeEmbedded.java
+++ celos-ci/src/main/java/com/collective/celos/ci/mode/test/TestRunCelosServerModeEmbedded.java
@@ -1,5 +1,6 @@
 package com.collective.celos.ci.mode.test;
 
+import com.collective.celos.Util;
 import com.collective.celos.ci.config.CiCommandLine;
 import com.collective.celos.ci.deploy.JScpWorker;
 import com.collective.celos.ci.deploy.WorkflowFilesDeployer;
@@ -56,7 +57,9 @@ public class TestRunCelosServerModeEmbedded implements TestRunCelosServerMode {
         defaultsDir.mkdirs();
         stateDatabase.mkdirs();
 
-        Integer port = celosServer.startServer(additionalJSParams, workflowsDir, defaultsDir, stateDatabase, stateDatabase);
+        Integer port = Util.getFreePort();
+
+        celosServer.startServer(port, additionalJSParams, workflowsDir, defaultsDir, stateDatabase);
 
         logJsFileExists(WorkflowFilesDeployer.WORKFLOW_FILENAME, testRun);
         logJsFileExists(WorkflowFilesDeployer.DEFAULTS_FILENAME, testRun);
