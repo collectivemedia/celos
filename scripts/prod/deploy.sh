7fc1f133bf9a796b4e05770376d191067d862609
Manuel Simoni <msimoni@gmail.com>
2015-09-03 14:07:28 +0200

    UI test deployment

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -4,6 +4,6 @@ set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
 export ANSIBLE_SSH_ARGS=""
 scripts/build-celos
-ansible-playbook scripts/prod/kinit.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
-ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json" -e service_version=${GIT_COMMIT}
-ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-ui     -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json" -e service_version=${GIT_COMMIT}
+ansible-playbook scripts/prod/kinit.yaml -c ssh -u celos-ci -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
+#ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos-ci -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json" -e service_version=${GIT_COMMIT}
+ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos-ci -i scripts/prod/conf/inventory-ui     -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json" -e service_version=${GIT_COMMIT}
