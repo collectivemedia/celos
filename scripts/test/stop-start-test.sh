46f7cb1d47a0ed62dc942a6128d9ef1edc4c40a8
Manuel Simoni <msimoni@gmail.com>
2015-09-03 09:28:48 +0200

    Use fast Ansible options

--- scripts/test/stop-start-test.sh
+++ scripts/test/stop-start-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-SERVER_PARAMS="-u celos-ci -i scripts/test/conf/inventory-server -e @scripts/test/conf/integration-params.json -e @scripts/conf/common-params-server.json"
-UI_PARAMS="    -u celos-ci -i scripts/test/conf/inventory-ui     -e @scripts/test/conf/integration-params.json -e @scripts/conf/common-params-ui.json"
+SERVER_PARAMS="-c ssh -u celos-ci -i scripts/test/conf/inventory-server -e @scripts/test/conf/integration-params.json -e @scripts/conf/common-params-server.json"
+UI_PARAMS="    -c ssh -u celos-ci -i scripts/test/conf/inventory-ui     -e @scripts/test/conf/integration-params.json -e @scripts/conf/common-params-ui.json"
 set -e
 set -x
 ansible-playbook scripts/prod/celos-check.yaml ${SERVER_PARAMS}
