cebceffb32198d044c581423de09837b7bd5da67
Manuel Simoni <msimoni@gmail.com>
2015-09-03 13:42:02 +0200

    Add missing class

---
+++ celos-ui/src/main/java/com/collective/celos/ui/UIConfiguration.java
@@ -0,0 +1,48 @@
+package com.collective.celos.ui;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableSet;
+
+import com.collective.celos.ScheduledTime;
+import com.collective.celos.Util;
+import com.collective.celos.WorkflowID;
+import com.collective.celos.WorkflowStatus;
+
+public class UIConfiguration {
+
+    private final ScheduledTime start;
+    private final ScheduledTime end;
+    private final NavigableSet<ScheduledTime> tileTimes;
+    private final List<WorkflowGroup> groups;
+    private final Map<WorkflowID, WorkflowStatus> statuses;
+    
+    public UIConfiguration(ScheduledTime start, ScheduledTime end, NavigableSet<ScheduledTime> tileTimes, List<WorkflowGroup> groups, Map<WorkflowID, WorkflowStatus> statuses) {
+        this.start = Util.requireNonNull(start);
+        this.end = Util.requireNonNull(end);
+        this.tileTimes = Util.requireNonNull(tileTimes);
+        this.groups = Util.requireNonNull(groups);
+        this.statuses = Util.requireNonNull(statuses);
+    }
+    
+    public ScheduledTime getStart() {
+        return start;
+    }
+
+    public ScheduledTime getEnd() {
+        return end;
+    }
+
+    public NavigableSet<ScheduledTime> getTileTimes() {
+        return tileTimes;
+    }
+
+    public List<WorkflowGroup> getGroups() {
+        return groups;
+    }
+
+    public Map<WorkflowID, WorkflowStatus> getStatuses() {
+        return statuses;
+    }
+
+}
