97b8dee0b52ec1e584a03a603462cf97585638c7
Manuel Simoni <msimoni@gmail.com>
2015-09-04 13:48:02 +0200

    Test /trigger-status servlet

--- celos-server/src/main/java/com/collective/celos/servlet/TriggerStatusServlet.java
+++ celos-server/src/main/java/com/collective/celos/servlet/TriggerStatusServlet.java
@@ -27,36 +27,25 @@ public class TriggerStatusServlet extends AbstractJSONServlet {
     private static Logger LOGGER = Logger.getLogger(TriggerStatusServlet.class);
 
     protected static final String ID_PARAM = "id";
-//    private static final String TIME_PARAM = "time";
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
         try {
             String id = req.getParameter(ID_PARAM);
             if (id == null) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ID_PARAM + " parameter missing.");
+                res.sendError(HttpServletResponse.SC_BAD_REQUEST, ID_PARAM + " parameter missing.");
                 return;
             }
             Scheduler scheduler = getOrCreateCachedScheduler();
             Workflow wf = scheduler.getWorkflowConfiguration().findWorkflow(new WorkflowID(id));
             if (wf == null) {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Workflow not found: " + id);
+                res.sendError(HttpServletResponse.SC_NOT_FOUND, "Workflow not found: " + id);
                 return;
             }
-            final String status = getStatus(req, response, scheduler, wf);
-            if (status != null) {
-                response.getOutputStream().write(status.getBytes());
-            }
+            TriggerStatus status = wf.getTrigger().getTriggerStatus(scheduler, ScheduledTime.now(), getRequestTime(req));
+            writer.writeValue(res.getOutputStream(), status);
         } catch (Exception e) {
             throw new ServletException(e);
         }
     }
-
-    protected String getStatus(HttpServletRequest req, HttpServletResponse response, Scheduler scheduler, Workflow wf) throws Exception {
-        ScheduledTime time = getRequestTime(req);
-        ScheduledTime now = ScheduledTime.now();
-        final Trigger trigger = wf.getTrigger();
-        final TriggerStatus statusObject = trigger.getTriggerStatus(scheduler, now, time);
-        // convert user object to json string, and save to a file
-        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(statusObject);
-    }
+    
 }
