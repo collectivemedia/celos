6d6477ec4e73a7ceff058c6537dc8ad9055a31cb
Alexander Konopko <alexander.konopko@gmail.com>
2015-09-08 14:05:06 +0300

    check if rerun Slot exists

--- celos-server/src/main/java/com/collective/celos/servlet/RerunServlet.java
+++ celos-server/src/main/java/com/collective/celos/servlet/RerunServlet.java
@@ -4,13 +4,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.collective.celos.*;
 import org.apache.log4j.Logger;
 
-import com.collective.celos.ScheduledTime;
-import com.collective.celos.SlotID;
-import com.collective.celos.SlotState;
-import com.collective.celos.StateDatabase;
-import com.collective.celos.WorkflowID;
+import java.util.Set;
 
 /**
  * Posting to this servlet reruns the specified slot.
@@ -36,13 +33,27 @@ public class RerunServlet extends AbstractServlet {
                 res.sendError(HttpServletResponse.SC_BAD_REQUEST, ID_PARAM + " parameter missing.");
                 return;
             }
-            SlotID slot = new SlotID(new WorkflowID(id), time);
-            StateDatabase db = getOrCreateCachedScheduler().getStateDatabase();
+
+            Scheduler scheduler = getOrCreateCachedScheduler();
+            WorkflowID workflowID = new WorkflowID(id);
+            Workflow workflow = scheduler.getWorkflowConfiguration().findWorkflow(workflowID);
+            if (workflow == null) {
+                res.sendError(HttpServletResponse.SC_NOT_FOUND, "Workflow not found: " + id);
+                return;
+            }
+
+            SlotID slot = new SlotID(workflowID, time);
+            Set<ScheduledTime> timeSet = workflow.getSchedule().getScheduledTimes(scheduler, time.minusSeconds(1), time.plusSeconds(1));
+            if (!timeSet.contains(time)) {
+                res.sendError(HttpServletResponse.SC_NOT_FOUND, "Slot is not found: " + slot);
+                return;
+            }
+
+            StateDatabase db = scheduler.getStateDatabase();
             SlotState state = db.getSlotState(slot);
             if (state != null) {
                 updateSlotToRerun(state, db);
             }
-            // FIXME: verify that slot is a valid slot wrt the schedule
             db.markSlotForRerun(slot, ScheduledTime.now());
         } catch(Exception e) {
             throw new ServletException(e);
