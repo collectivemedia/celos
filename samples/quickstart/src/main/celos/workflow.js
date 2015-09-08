a68778b80b46df4e9fbf1d42894a6a651142c446
Manuel Simoni <msimoni@gmail.com>
2015-09-08 15:26:08 +0200

    More docs

--- samples/quickstart/src/main/celos/workflow.js
+++ samples/quickstart/src/main/celos/workflow.js
@@ -1,4 +1,4 @@
-celos.importDefaults("quickstart-settings");
+celos.importDefaults("wordcount");
 
 var INPUT_PATH = "/user/" + CELOS_USER + "/celos/quickstart/input/${year}-${month}-${day}/${hour}00";
 var OUTPUT_PATH = "/user/" + CELOS_USER + "/celos/quickstart/output/${year}-${month}-${day}/${hour}00";
