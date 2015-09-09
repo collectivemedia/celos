b07404d83afcbdff08ae0d123743bc3b1ff7c42b
Oleg Baskakov <obaskakov@collective.com>
2015-09-09 10:40:45 -0400

    License script for all java and javascript files #604

---
+++ scripts/add-license.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set x
+
+for x in $(find . -name *.js -o -name *.java)
+do
+    if [ "$(grep '* Copyright 2015 Collective, Inc.' ${x})" ]
+    then
+        echo skipping ${x}
+    else
+        cp ./scripts/templates/license.tpl tmp.txt
+        cat ${x} >> tmp.txt
+        mv tmp.txt ${x}
+    fi
+done
