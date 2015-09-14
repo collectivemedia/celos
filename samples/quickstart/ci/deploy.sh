9e893e311071b109dd41fb8c9024dba65520fa63
Manuel Simoni <msimoni@gmail.com>
2015-09-14 07:06:49 -0400

    Bring build and deploy dir creation into right order

--- samples/quickstart/ci/deploy.sh
+++ samples/quickstart/ci/deploy.sh
@@ -2,16 +2,18 @@ set -x
 set -e
 export HDFS_ROOT=$1
 
-# Put deploy dir into build/
+# Build JAR
+./gradlew clean jar
+
+# Create deploy dir at build/celos-deploy
 export DEPLOY_DIR=build/celos-deploy
 rm -rf $DEPLOY_DIR
 mkdir -p $DEPLOY_DIR/hdfs/lib
 
-# Build and copy files into deploy dir
-./gradlew clean jar
+# Copy files into deploy dir
 cp src/main/celos/* $DEPLOY_DIR/
 cp src/main/oozie/* $DEPLOY_DIR/hdfs/
 cp build/libs/* $DEPLOY_DIR/hdfs/lib/
 
 # Run Celos CI
-java -jar ../../celos-ci/build/libs/celos-ci-fat.jar --workflowName wordcount --mode deploy --deployDir $DEPLOY_DIR --target $TARGET --hdfsRoot $HDFS_ROOT
+java -jar ../../celos-ci/build/libs/celos-ci-fat.jar --workflowName wordcount --mode deploy --deployDir $DEPLOY_DIR --target $TARGET_FILE --hdfsRoot $HDFS_ROOT
