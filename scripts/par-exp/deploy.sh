e462f5d232ac03f8aa94a89f64e7ee36ae557263
Manuel Simoni <msimoni@gmail.com>
2015-10-21 17:46:30 +0200

    Don't deploy UI

--- scripts/par-exp/deploy.sh
+++ scripts/par-exp/deploy.sh
@@ -5,5 +5,3 @@ set -e
 export ANSIBLE_SSH_ARGS=""
 scripts/build.sh
 ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos-ci -i scripts/par-exp/conf/inventory-server -e "@scripts/par-exp/conf/celos-params.json" -e "@scripts/conf/common-params-server.json" -e service_version=${GIT_COMMIT}
-ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos-ci -i scripts/par-exp/conf/inventory-ui     -e "@scripts/par-exp/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json" -e service_version=${GIT_COMMIT}
-
