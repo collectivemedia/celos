73379fb7461625ce3f1ae4517a98d4acd7616ddd
Alexander Konopko <alexander.konopko@gmail.com>
2015-11-05 19:25:13 +0200

    fix

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -7,4 +7,4 @@ scripts/build.sh
 ansible-playbook scripts/prod/kinit.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
 ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json" -e service_version=${GIT_COMMIT}
 ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-ui     -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json" -e service_version=${GIT_COMMIT}
-./gradlew celos:uploadArchives celos-ci:uploadArchives
+./gradlew celos-server:uploadArchives celos-ci:uploadArchives
