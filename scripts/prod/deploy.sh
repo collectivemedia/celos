8af8f9e4bf8ba096e3f580cff2b2d2e86845665e
Alexander Konopko <alexander.konopko@gmail.com>
2015-11-05 20:36:40 +0200

    try 2

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -7,4 +7,4 @@ export ANSIBLE_SSH_ARGS=""
 #ansible-playbook scripts/prod/kinit.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json"
 #ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-server -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-server.json" -e service_version=${GIT_COMMIT}
 #ansible-playbook scripts/celos-deploy.yaml -c ssh -u celos -i scripts/prod/conf/inventory-ui     -e "@scripts/prod/conf/celos-params.json" -e "@scripts/conf/common-params-ui.json" -e service_version=${GIT_COMMIT}
-./gradlew celos-ci:uploadArchives
+./gradlew celos-server:uploadArchives celos-ci:uploadArchives
