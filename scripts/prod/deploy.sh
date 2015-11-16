40ad95cd492f691d1fe8c9fb3816b521c5e4caf2
Alexander Konopko <alexander.konopko@gmail.com>
2015-11-16 23:02:57 +0200

    refresh dependencies for celos jar version check

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -12,4 +12,4 @@ MINUTES_JAN01_2015=23667720
 MINUTES_SINCE_JAN01_2015=$(($(date +%s) / 60 - MINUTES_JAN01_2015))
 export CELOS_BUILD_NUMBER=$MINUTES_SINCE_JAN01_2015
 ./gradlew clean celos-server:uploadArchives celos-ci:uploadArchives
-(cd scripts/test/check-upload && ./gradlew checkVersion)
+(cd scripts/test/check-upload && ./gradlew checkVersion --refresh-dependencies)
