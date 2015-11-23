c15c544d271644fb0d46e1e4fb1e0e74fe5d4733
Alexander Konopko <alexander.konopko@gmail.com>
2015-11-23 20:31:58 +0200

    fix tests

--- celos-server/src/test/java/com/collective/celos/server/ServerCommandLineParserTest.java
+++ celos-server/src/test/java/com/collective/celos/server/ServerCommandLineParserTest.java
@@ -20,12 +20,26 @@ import com.collective.celos.database.FileSystemStateDatabase;
 import com.collective.celos.database.JDBCStateDatabase;
 import com.collective.celos.database.StateDatabase;
 import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ServerCommandLineParserTest {
-    
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    private File tmpDir;
+
+    @Before
+    public void setUp() throws IOException {
+        tmpDir = folder.newFolder();
+    }
+
     @Test(expected=IllegalArgumentException.class)
     public void testPortIsRequired() throws Exception {
         new ServerCommandLineParser().parse(new String[] {});
@@ -35,27 +49,32 @@ public class ServerCommandLineParserTest {
     public void testPortIsRequiredEvenIfOtherArgsAreSupplied() throws Exception {
         new ServerCommandLineParser().parse(new String[] {"--defaults", "/foo"});
     }
-    
+
+    @Test (expected = IOException.class)
+    public void testDefaultsNoDb() throws Exception {
+        new ServerCommandLineParser().parse(new String[] {"--port", "1337" });
+    }
+
     @Test
     public void testDefaults() throws Exception {
-        ServerCommandLine cmdLine = new ServerCommandLineParser().parse(new String[] {"--port", "1337"});
+        ServerCommandLine cmdLine = new ServerCommandLineParser().parse(new String[] {"--port", "1337", "--db", tmpDir.getAbsolutePath() });
         Assert.assertEquals(1337, cmdLine.getPort());
         Assert.assertEquals(new File(Constants.DEFAULT_WORKFLOWS_DIR), cmdLine.getWorkflowsDir());
         Assert.assertEquals(new File(Constants.DEFAULT_DEFAULTS_DIR), cmdLine.getDefaultsDir());
         Assert.assertEquals(StateDatabase.DatabaseType.FILESYSTEM, cmdLine.getDatabase().getDatabaseType());
         FileSystemStateDatabase db = (FileSystemStateDatabase) cmdLine.getDatabase();
-        Assert.assertEquals(new File(Constants.DEFAULT_DB_DIR), db.getDir());
+        Assert.assertEquals(db.getDir(), tmpDir);
     }
 
     @Test
     public void testOverrideDefaults() throws Exception {
-        ServerCommandLine cmdLine = new ServerCommandLineParser().parse(new String[] {"--port", "1337", "--workflows", "/wf", "--db", "/db", "--defaults", "/defaults"});
+        ServerCommandLine cmdLine = new ServerCommandLineParser().parse(new String[] {"--port", "1337", "--workflows", "/wf", "--db", tmpDir.getAbsolutePath(), "--defaults", "/defaults"});
         Assert.assertEquals(1337, cmdLine.getPort());
         Assert.assertEquals(new File("/wf"), cmdLine.getWorkflowsDir());
         Assert.assertEquals(new File("/defaults"), cmdLine.getDefaultsDir());
         Assert.assertEquals(StateDatabase.DatabaseType.FILESYSTEM, cmdLine.getDatabase().getDatabaseType());
         FileSystemStateDatabase db = (FileSystemStateDatabase) cmdLine.getDatabase();
-        Assert.assertEquals(new File("/db"), db.getDir());
+        Assert.assertEquals(tmpDir, db.getDir());
     }
 
     @Test (expected = IllegalArgumentException.class)
