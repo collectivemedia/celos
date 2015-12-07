d8ab57b0f55ee6cb975b2ff366caa49472d594a7
Oleg Baskakov <obaskakov@collective.com>
2015-12-07 16:02:16 -0500

    extra arg for conflux db added

--- scripts/deploy-conflux.sh
+++ scripts/deploy-conflux.sh
@@ -7,10 +7,12 @@ export ANSIBLE_SSH_ARGS=""
 #scripts/build.sh
 #ansible-playbook scripts/playbooks/kinit.yaml -c ssh -u ${CELOS_USER} -i ${INVENTORY_SERVER}
 
+ACTION=deploy
+
 export INVENTORY_SERVER=scripts/inventory/conflux-server
 export INVENTORY_UI=scripts/inventory/conflux-ui
-./scripts/server-and-ui-action.sh stop
+./scripts/server-and-ui-action.sh ${ACTION}
 
 export INVENTORY_SERVER=scripts/inventory/testing-server
 export INVENTORY_UI=scripts/inventory/testing-ui
-./scripts/server-and-ui-action.sh stop
+./scripts/server-and-ui-action.sh ${ACTION}
--- scripts/templates/service-start.tpl
+++ scripts/templates/service-start.tpl
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CELOS_VERSION={{ service_version }} exec java 2>&1 -cp "{{ dest_jar_path }}:/etc/hadoop/conf" {{ main_class }} --port {{ service_port }} {{ service_args }}
+CELOS_VERSION={{ service_version }} exec java 2>&1 -cp "{{ dest_jar_path }}:/etc/hadoop/conf" {{ main_class }} --port {{ service_port }} {{ service_args }} {{ extra_args }}
