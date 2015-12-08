77f25fdf77eb5a6ee4e88e55b8ad3e7b45da9cdf
Oleg Baskakov <obaskakov@collective.com>
2015-12-08 10:22:20 -0500

    copy conflux into deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,14 @@ export INVENTORY_UI=scripts/inventory/production-ui
 export ANSIBLE_SSH_ARGS=""
 scripts/build.sh
 ansible-playbook scripts/playbooks/kinit.yaml -c ssh -u ${CELOS_USER} -i ${INVENTORY_SERVER}
+
+ACTION=deploy
+
 ./scripts/server-and-ui-action.sh deploy
+
+export INVENTORY_SERVER=scripts/inventory/conflux-server
+export INVENTORY_UI=scripts/inventory/conflux-ui
+./scripts/server-and-ui-action.sh ${ACTION}
 
 MINUTES_JAN01_2015=23667720
 MINUTES_SINCE_JAN01_2015=$(($(date +%s) / 60 - MINUTES_JAN01_2015))
