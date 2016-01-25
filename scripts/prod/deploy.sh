9a90662fa3c0ab25a2c49a97d34e772c45e374f7
Oleg Baskakov <obaskakov@collective.com>
2016-01-25 15:40:36 -0500

    create scripts/deploy-testing.sh

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -4,16 +4,17 @@ set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
 
 export CELOS_USER=celos
-export INVENTORY_SERVER=scripts/inventory/testing-server
-export INVENTORY_UI=scripts/inventory/testing-ui
+export INVENTORY_SERVER=scripts/inventory/production-server
+export INVENTORY_UI=scripts/inventory/production-ui
 export ANSIBLE_SSH_ARGS=""
 
 scripts/build.sh
 
 scripts/server-and-ui-action.sh deploy
 
-#MINUTES_JAN01_2015=23667720
-#MINUTES_SINCE_JAN01_2015=$(($(date +%s) / 60 - MINUTES_JAN01_2015))
-#export CELOS_BUILD_NUMBER=$MINUTES_SINCE_JAN01_2015
-#./gradlew clean celos-server:uploadArchives celos-ci:uploadArchives celos-common:uploadArchives
-#(cd scripts/test/check-upload && ./gradlew checkVersion --refresh-dependencies)
+
+MINUTES_JAN01_2015=23667720
+MINUTES_SINCE_JAN01_2015=$(($(date +%s) / 60 - MINUTES_JAN01_2015))
+export CELOS_BUILD_NUMBER=$MINUTES_SINCE_JAN01_2015
+./gradlew clean celos-server:uploadArchives celos-ci:uploadArchives celos-common:uploadArchives
+(cd scripts/test/check-upload && ./gradlew checkVersion --refresh-dependencies)
