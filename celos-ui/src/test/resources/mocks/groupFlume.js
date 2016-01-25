a7b73f0d9dd5d0aeb069b90618ecafb43bf84647
Oleg Baskakov <obaskakov@collective.com>
2016-01-25 12:21:16 -0500

    js files for new UI with hardcoded npm dependencies

---
+++ celos-ui/src/test/resources/mocks/groupFlume.js
@@ -0,0 +1,29 @@
+var MOCK_GROUP_FLUME = {
+  "name" : "Flume",
+  "times" : [ "0800", "0700", "0600", "0500"],
+  "days" : [ null, null, null, null],
+  "rows" : [ {
+    "workflowName" : "flume-ready-sv4",
+    "rows" : [ {
+      "status" : "WAITING",
+      "url" : null,
+      "timestamps" : [ "2015-12-18T08:00Z" ],
+      "quantity" : 1
+    }, {
+      "status" : "RUNNING",
+      "url" : "http://cldmgr001.ewr004.collective-media.net:8888/oozie/list_oozie_workflow/0059046-151129084925398-oozie-oozi-W",
+      "timestamps" : [ "2015-12-18T07:00Z" ],
+      "quantity" : 1
+    }, {
+      "status" : "SUCCESS",
+      "url" : "http://cldmgr001.ewr004.collective-media.net:8888/oozie/list_oozie_workflow/0059008-151129084925398-oozie-oozi-W",
+      "timestamps" : [ "2015-12-18T06:00Z" ],
+      "quantity" : 1
+    }, {
+      "status" : "SUCCESS",
+      "url" : "http://cldmgr001.ewr004.collective-media.net:8888/oozie/list_oozie_workflow/0058640-151129084925398-oozie-oozi-W",
+      "timestamps" : [ "2015-12-18T05:00Z" ],
+      "quantity" : 1
+    }]
+  } ]
+};
