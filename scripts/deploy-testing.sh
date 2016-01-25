d8a8e2c21890a6cc98929bf74d23e36eee3554df
Oleg Baskakov <obaskakov@collective.com>
2016-01-25 18:36:38 -0500

    Update deploy-testing.sh
    Dead code elimination.

--- scripts/deploy-testing.sh
+++ scripts/deploy-testing.sh
@@ -11,9 +11,3 @@ export ANSIBLE_SSH_ARGS=""
 scripts/build.sh
 
 scripts/server-and-ui-action.sh deploy
-
-#MINUTES_JAN01_2015=23667720
-#MINUTES_SINCE_JAN01_2015=$(($(date +%s) / 60 - MINUTES_JAN01_2015))
-#export CELOS_BUILD_NUMBER=$MINUTES_SINCE_JAN01_2015
-#./gradlew clean celos-server:uploadArchives celos-ci:uploadArchives celos-common:uploadArchives
-#(cd scripts/test/check-upload && ./gradlew checkVersion --refresh-dependencies)
