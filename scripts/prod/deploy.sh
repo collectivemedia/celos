5b69963260bf43a6d52bafd789606187b9489735
Oleg Baskakov <obaskakov@collective.com>
2016-01-27 11:49:35 -0500

    second celos instance

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -4,13 +4,19 @@ set -e
 [[ -z ${GIT_COMMIT} ]] && echo pls specify GIT_COMMIT && exit 1
 
 export CELOS_USER=celos
-export INVENTORY_SERVER=scripts/inventory/production-server
-export INVENTORY_UI=scripts/inventory/production-ui
 export ANSIBLE_SSH_ARGS=""
 
 scripts/build.sh
 
-scripts/server-and-ui-action.sh deploy
+ACTION=deploy
+
+export INVENTORY_SERVER=scripts/inventory/production-server
+export INVENTORY_UI=scripts/inventory/production-ui
+scripts/server-and-ui-action.sh ${ACTION}
+
+export INVENTORY_SERVER=scripts/inventory/conflux-server
+export INVENTORY_UI=scripts/inventory/conflux-ui
+./scripts/server-and-ui-action.sh ${ACTION}
 
 
 MINUTES_JAN01_2015=23667720
