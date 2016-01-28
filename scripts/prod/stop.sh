43e91774edb585b191c53d05fbf5edcea8b3cdba
Alex Konopko <alexander.konopko@gmail.com>
2016-01-28 23:07:49 +0200

    setup one more server

--- scripts/prod/stop.sh
+++ scripts/prod/stop.sh
@@ -5,3 +5,7 @@ export CELOS_USER=celos
 export INVENTORY_SERVER=scripts/inventory/production-server
 export INVENTORY_UI=scripts/inventory/production-ui
 ./scripts/server-and-ui-action.sh stop
+
+export INVENTORY_SERVER=scripts/inventory/jdbc-production-server
+export INVENTORY_UI=scripts/inventory/jdbc-production-ui
+./scripts/server-and-ui-action.sh stop
